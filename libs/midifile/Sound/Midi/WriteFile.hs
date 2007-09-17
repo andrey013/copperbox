ec37cbfa72d4c4a7600fbe837a37bee7c13678d5
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2007-09-17 21:28:03 +0000

    Adding changes to MidiWrite.hs to account for datatype changes (these should have gone in yesterday). Adding a new file DemoTrack.hs that outputs a midi file.

--- libs/midifile/Sound/Midi/WriteFile.hs
+++ libs/midifile/Sound/Midi/WriteFile.hs
@@ -16,8 +16,6 @@ import qualified Data.ByteString.Char8 as SBS
 
 writeMidi :: FilePath -> MidiFile -> IO ()
 writeMidi path mf = do 
-  error "here" 
-{-  
   let lbs = runPut (putMidiFile mf)
   L.writeFile path lbs     
                     
@@ -40,7 +38,7 @@ putHeader (Header fmt n td) = do
 putTrack :: Track -> Put
 putTrack (Track es) = do
   putString     "MTrk"
-  let contents = runPut (mapM_ putEvent es)
+  let contents = runPut (mapM_ putMessage es)
   let len = L.length contents
   putWord32be (fromIntegral len)
   putLazyByteString contents
@@ -66,74 +64,82 @@ putTextType LYRICS             = putWord8 5
 putTextType MARKER             = putWord8 6
 putTextType CUE_POINT          = putWord8 7
 
-putEvent :: Event -> Put
-putEvent (Event dt ty) = do
+putMessage :: Message -> Put
+putMessage (dt,evt) = do
   putVarlen dt
-  putEventType ty
+  putEvent  evt
+
+putEvent :: Event -> Put
+putEvent (VoiceEvent  evt) = putVoiceEvent evt
+putEvent (SystemEvent evt) = putSystemEvent evt
+putEvent (MetaEvent   evt) = putMetaEvent evt
   
-putEventType :: EventType -> Put 
-putEventType (NoteOff ch nt vel) = do
+    
+putVoiceEvent :: VoiceEvent -> Put 
+putVoiceEvent (NoteOff ch nt vel) = do
   putWord8 (0x8 `chShift` ch)
   putWord8 nt
   putWord8 vel 
   
-putEventType (NoteOn ch nt vel) = do
+putVoiceEvent (NoteOn ch nt vel) = do
   putWord8 (0x9 `chShift` ch)
   putWord8 nt
   putWord8 vel 
   
-putEventType (NoteAftertouch ch nt val) = do
+putVoiceEvent (NoteAftertouch ch nt val) = do
   putWord8 (0xA `chShift` ch)
   putWord8 nt
   putWord8 val
   
-putEventType (Controller ch nt val) = do
+putVoiceEvent (Controller ch nt val) = do
   putWord8 (0xB `chShift` ch)
   putWord8 nt
   putWord8 val
   
-putEventType (ProgramChange ch num) = do
+putVoiceEvent (ProgramChange ch num) = do
   putWord8 (0xC `chShift` ch)
   putWord8 num
   
-putEventType (ChanAftertouch ch val) = do
+putVoiceEvent (ChanAftertouch ch val) = do
   putWord8 (0xD `chShift` ch)
   putWord8 val
   
-putEventType (PitchBend ch val) = do
+putVoiceEvent (PitchBend ch val) = do
   putWord8 ( (0xE::Word8) `chShift` ch)
   putWord16be val              -- 2DO
-  
-putEventType (TextEvent ty cs) = do
+
+
+putMetaEvent :: MetaEvent -> Put
+putMetaEvent (TextEvent ty cs) = do
   putWord8      0xFF
   putTextType   ty
   putVarlen     (length cs)
   putByteString (SBS.pack cs)
   
-putEventType (SequenceNumber n) = do
+putMetaEvent (SequenceNumber n) = do
   putWord8      0xFF
   putWord8      0
   putWord8      2
   putWord16be   n
   
-putEventType (ChannelPrefix ch) = do
+putMetaEvent (ChannelPrefix ch) = do
   putWord8 0xFF
   putWord8 0x20
   putWord8 1
   putWord8 ch
   
-putEventType (EndOfTrack) = do
+putMetaEvent (EndOfTrack) = do
   putWord8 0xFF
   putWord8 0x2F
   putWord8 0
   
-putEventType (SetTempo t) = do
+putMetaEvent (SetTempo t) = do
   putWord8 0xFF
   putWord8 0x51
   putWord8 3           
   putWord24be t
   
-putEventType (SMPTEOffset hr mn sc fr sfr) = do
+putMetaEvent (SMPTEOffset hr mn sc fr sfr) = do
   putWord8 0xFF
   putWord8 0x54
   putWord8 5
@@ -143,7 +149,7 @@ putEventType (SMPTEOffset hr mn sc fr sfr) = do
   putWord8 fr
   putWord8 sfr
   
-putEventType (TimeSignature nmr dnm met nps) = do
+putMetaEvent (TimeSignature nmr dnm met nps) = do
   putWord8 0xFF
   putWord8 0x58   
   putWord8 4
@@ -152,18 +158,19 @@ putEventType (TimeSignature nmr dnm met nps) = do
   putWord8 met
   putWord8 nps    
   
-putEventType (KeySignature ky sc) = do
+putMetaEvent (KeySignature ky sc) = do
   putWord8 0xFF
   putWord8 0x59
   putWord8 2 
   putWord8 (wrapint ky)
   putScale sc
 
-putEventType (SSME i bs) = do
+putMetaEvent (SSME i bs) = do
   putVarlen i
   putByteString bs
-    
-putEventType (Sysex i bs) = do
+
+putSystemEvent :: SystemEvent -> Put    
+putSystemEvent (SysEx i bs) = do
   putVarlen i
   putByteString bs
   
@@ -175,7 +182,7 @@ putScale MINOR = putWord8 1
 putVarlen :: Integral a => a -> Put
 putVarlen i = mapM_ putWord8 (varlen i)
 
--}
+
 
 --------------------------------------------------------------------------------
 -- Helpers 
