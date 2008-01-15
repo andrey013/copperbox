939b31ef5db7919410df59ac321e62ea41b6e58c
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2008-01-15 22:37:32 +0000

    Changed the ABC parser back to use Parsec rather than ReadP.

--- bala/Bala/Format/Abc/Parser.hs
+++ bala/Bala/Format/Abc/Parser.hs
@@ -16,30 +16,31 @@ module Bala.Format.Abc.Parser where
 
 import Bala.Format.Abc.Datatypes
 
-import Control.Applicative hiding (many, optional)
+import Control.Applicative hiding (many, optional, (<|>) )
 import Control.Monad
 
-import Data.Char hiding (Space)
+import Data.Char
 import Data.List (sortBy)
-import Text.ParserCombinators.ReadP
+import Text.ParserCombinators.Parsec hiding ( {- token, -} space)
 
 
 
-instance Applicative ReadP where
+
+instance Applicative (GenParser tok st) where
   pure = return
   (<*>) = ap
    
 
-parse = readP_to_S
+{- parse = readP_to_S -}
 
   
 
 
-abcFile :: ReadP [AbcFileElement]
-abcFile = repeated fileElement
+abcFile :: Parser [AbcFileElement]
+abcFile = many1 fileElement
 
-fileElement :: ReadP AbcFileElement
-fileElement = choiceB [ fieldsElement, tuneElement, texElement, 
+fileElement :: Parser AbcFileElement
+fileElement = choice [ fieldsElement, tuneElement, texElement, 
                         dropComment, dropFeed]
   where fieldsElement = AbcFileFieldsElement <$> fileFields 
         tuneElement   = AbcTuneElement       <$> abcTune
@@ -51,22 +52,22 @@ fileElement = choiceB [ fieldsElement, tuneElement, texElement,
   
   
 
-fileFields :: ReadP [AbcField]
+fileFields :: Parser [AbcField]
 fileFields = many1 $ choice 
                     [ fieldFile,      fieldBook,        fieldGroup 
                     , fieldHistory,   fieldInformation, fieldMeter 
                     , fieldOrigin,    fieldRhythm
                     ]
 
-fieldFile :: ReadP AbcField
+fieldFile :: Parser AbcField
 fieldFile = readField 'F' text AbcFileField
 
-abcTune :: ReadP AbcTune
+abcTune :: Parser AbcTune
 abcTune = (,) <$> abcHeader <*> abcMusic
 
   
 
-abcHeader :: ReadP AbcHeader
+abcHeader :: Parser AbcHeader
 abcHeader = AbcHeader <$> fieldNumber <*  optional comment <*> many1 fieldTitle
                                       <*> many otherFields <*> fieldKey
  
@@ -77,7 +78,7 @@ fieldNumber = readField 'X' readNat AbcNumberField
 fieldTitle  = readField 'T' text AbcTitleField  
 
 
-otherFields :: ReadP AbcField
+otherFields :: Parser AbcField
 otherFields = choice [ fieldArea,         fieldBook,        fieldComposer 
                      , fieldDiscography,  fieldElemskip,    fieldGroup
                      , fieldHistory,      fieldInformation, fieldDefaultLength
@@ -86,7 +87,7 @@ otherFields = choice [ fieldArea,         fieldBook,        fieldComposer
                      , fieldSource,       fieldTranscrnotes
                      ]
   
-  {- +++ comment -}
+  {- <|> comment -}
 
 readField ch fn constr = constr <$> (char ch *> char ':' *> fn <* endOfLine)
    
@@ -112,16 +113,16 @@ fieldKey            = readField 'K' key   AbcKeyField
 
        
 
-key :: ReadP AbcKey
-key = keySpec +++ hp
+key :: Parser AbcKey
+key = keySpec <|> hp
   where
-    hp = char 'H' *> (pu +++ pl)
+    hp = char 'H' *> (pu <|> pl)
     pu = AbcHighlandNoKey      <$ char 'P'
     pl = AbcHighlandMixolydian <$ char 'p'
     
 
 
-keySpec :: ReadP AbcKey
+keySpec :: Parser AbcKey
 keySpec = AbcKey <$> inner
   where inner      = (,,) <$> keynote <*> (pmaybe modeSpec) <*>  many accidental
         accidental = char ' ' *> globalAccidental
@@ -129,26 +130,26 @@ keySpec = AbcKey <$> inner
 
 keynote = (,) <$> basenote <*> pmaybe keyAccidental
 
-keyAccidental :: ReadP AbcKeyAccidental
-keyAccidental = sharp <++ flat
+keyAccidental :: Parser AbcKeyAccidental
+keyAccidental = sharp <|> flat
   where sharp = AbcSharpKey <$ char '#'
         flat  = AbcFlatKey  <$ char 'b'
 
    
-modeSpec :: ReadP Mode_spec    
+modeSpec :: Parser Mode_spec    
 modeSpec = (,) <$> ospaceMode <*> extratext 
   where ospaceMode = pmaybe (char ' ') *> mode
 
-extratext :: ReadP String
-extratext = munch isAlpha
+extratext :: Parser String
+extratext = many (satisfy isAlpha)
 
 
-globalAccidental :: ReadP Global_accidental
+globalAccidental :: Parser Global_accidental
 globalAccidental = (,) <$> accidental <*> basenote
 
 
-mode :: ReadP AbcMode
-mode = choiceB [ modeMajor, modeLydian, modeIonian, modeMixolydian, 
+mode :: Parser AbcMode
+mode = choice [ modeMajor, modeLydian, modeIonian, modeMixolydian, 
                  modeDorian, modeAeolian, modePhrygian,
                  modeLocrian, modeMinor]
   where
@@ -165,21 +166,21 @@ mode = choiceB [ modeMajor, modeLydian, modeIonian, modeMixolydian,
 -- maybe modes should handle full names e.g. Gmajor
 
 
-meter :: ReadP AbcMeter
-meter = cutTime <++ commonTime <++ meterFraction
+meter :: Parser AbcMeter
+meter = cutTime <|> commonTime <|> meterFraction
   where cutTime     = AbcCutTimeMeter    <$ string "C|"
         commonTime  = AbcCommonTimeMeter <$ char 'C'
 
 
 
-meterFraction :: ReadP AbcMeter
+meterFraction :: Parser AbcMeter
 meterFraction = AbcMeter <$> readNat <* char '/' <*> readNat
  
 
 
 -- left rec ??
-tempo :: ReadP AbcTempo
-tempo = ta <++ tb <++ tc
+tempo :: Parser AbcTempo
+tempo = ta <|> tb <|> tc
   where eqk = char '=' *> readNat
   
         ta = AbcCTempo <$> (char 'C' *> noteLength) <*> eqk
@@ -188,32 +189,32 @@ tempo = ta <++ tb <++ tc
              
         tc = AbcTempo <$> readNat
 
-noteLengthStrict :: ReadP Note_length_strict
+noteLengthStrict :: Parser Note_length_strict
 noteLengthStrict = (,) <$> readNat <*> (char '/' *> readNat)
 
 
 
 
-parts :: ReadP [AbcPart]
+parts :: Parser [AbcPart]
 parts = many1 partSpec
 
 
-partSpec :: ReadP AbcPart
-partSpec = unaryPart <++ nestedPart <++ repeatedPart
+partSpec :: Parser AbcPart
+partSpec = unaryPart <|> nestedPart <|> repeatedPart
   where unaryPart     = AbcPartElem 0 <$> part 
         
         nestedPart    = AbcPartTree <$> readNat <*> parens (many1 partSpec)
         
         repeatedPart  = AbcPartElem <$> readNat <*> part
 
-part :: ReadP Char
+part :: Parser Char
 part = satisfy $ (flip elem) ['A'..'Z']
 
 
 
-endOfLine :: ReadP ()
+endOfLine :: Parser ()
 endOfLine = () <$ white *> optional trailingComment *> linefeed
-  where white           = munch (\c -> c == ' ' || c == '\t')
+  where white           = many $ satisfy (\c -> c == ' ' || c == '\t')
         trailingComment = char '%' *> text
   
 
@@ -223,51 +224,51 @@ endOfLine = () <$ white *> optional trailingComment *> linefeed
 
 parseElems = many1 element
 
-abcMusic :: ReadP AbcMusic
+abcMusic :: Parser AbcMusic
 abcMusic = (many1 abcLine) <* linefeed
 
 
-abcLine :: ReadP AbcLine
-abcLine = elementList +++ mtf +++ tcmd
+abcLine :: Parser AbcLine
+abcLine = elementList <|> mtf <|> tcmd
   where elementList = AbcElements      <$> many1 element <* lineEnder
         mtf         = AbcMidTuneField  <$> midTuneField
         tcmd        = AbcMidTexCommand <$> texCommand
 
-element :: ReadP AbcElement
+element :: Parser AbcElement
 element = choice [ noteElem, tupletElem, barElem, repElem, slurElem,
-                   spaceElem ]  -- +++ userElem
+                   spaceElem ]  -- <|> userElem
   where noteElem    = AbcNoteElement      <$> noteElement
         tupletElem  = AbcTupletElement    <$> tupletSpec <*> many1 noteElement
         barElem     = AbcBarlineElement   <$> barline
         repElem     = AbcRepeatElement    <$> nthRepeat
-        slurElem    = AbcSlurElement      <$> beginSlur +++ endSlur
+        slurElem    = AbcSlurElement      <$> (beginSlur <|> endSlur)
         spaceElem   = AbcSpaceElement     <$  space
         userElem    = undefined  -- AbcUserDefinedElement
 
 
 
       
-lineEnder :: ReadP ()
-lineEnder = comment +++ linefeed +++ lineBreak +++ noLineBreak 
+lineEnder :: Parser ()
+lineEnder = comment <|> linefeed <|> lineBreak <|> noLineBreak 
   
 tupletElement = (,) <$> tupletSpec <*> many1 noteElement
 
 
-tupletSpec :: ReadP Tuplet_spec
+tupletSpec :: Parser Tuplet_spec
 tupletSpec = (char '(') *> tups 
   where tups = readNat `sepBy` (char ':')
 
 
-noteElement :: ReadP Note_element
+noteElement :: Parser Note_element
 noteElement = (,) <$> noteStem <*> pmaybe brokenRhythm
 
 
-noteStem :: ReadP Note_stem
+noteStem :: Parser Note_stem
 noteStem = build <$> pmaybe guitarChord <*> pmaybe graceNotes <*> many gracings
                                         <*> noteOrMulti
 
   where 
-    noteOrMulti = (Left <$> note) <++ (Right <$> multiNote)
+    noteOrMulti = (Left <$> note) <|> (Right <$> multiNote)
     
     build mgc mgn gs (Left n)   = (mgc, mgn, gs, [n])
     build mgc mgn gs (Right ns) = (mgc, mgn, gs, ns)
@@ -275,41 +276,41 @@ noteStem = build <$> pmaybe guitarChord <*> pmaybe graceNotes <*> many gracings
 
 
 
-multiNote :: ReadP [AbcNote]
+multiNote :: Parser [AbcNote]
 multiNote = squares $ many1 note
   where squares = between (char '[') (char ']')
 
 
-note :: ReadP AbcNote
+note :: Parser AbcNote
 note = AbcNote <$> noteOrRest <*> pmaybe noteLength <*> pmaybe tie
 
 
 
 
-noteOrRest :: ReadP AbcValue
+noteOrRest :: Parser AbcValue
 noteOrRest = choice [ (AbcPitchValue <$> pitch), (AbcRest <$ rest) ]
 
 
 
 
-pitch :: ReadP AbcPitch
+pitch :: Parser AbcPitch
 pitch = build <$> pmaybe accidental <*> basenote <*> pmaybe octave
   where build ma n mo  = AbcPitch n ma mo
 
 
-octave :: ReadP AbcOctave    
-octave = lowOctave <++ highOctave
+octave :: Parser AbcOctave    
+octave = lowOctave <|> highOctave
   where lowOctave   = AbcLowOctave  <$> counting1 (char ',')
         highOctave  = AbcHighOctave <$> counting1 (char '\'')
 
 
 
-noteLength :: ReadP Note_length    
+noteLength :: Parser Note_length    
 noteLength = (,) <$> readNat <*> pmaybe readNat
 
 
-accidental :: ReadP AbcAccidental
-accidental = choiceB [dblSharp, sharp, dblFlat, flat]
+accidental :: Parser AbcAccidental
+accidental = choice [dblSharp, sharp, dblFlat, flat]
   where dblSharp = AbcDoubleSharp <$ string "^^" 
         sharp    = AbcSharp       <$ string "^"  
         dblFlat  = AbcDoubleFlat  <$ string "__" 
@@ -317,56 +318,56 @@ accidental = choiceB [dblSharp, sharp, dblFlat, flat]
         natural  = AbcNatural     <$ string "="
 
 
-basenote :: ReadP Basenote
+basenote :: Parser Basenote
 basenote = satisfy ((flip elem) "CDEFGABcdefgab")
 
  
-rest :: ReadP AbcValue
+rest :: Parser AbcValue
 rest = AbcRest <$ char 'z'
 
 
 
-brokenRhythm :: ReadP AbcBrokenRhythm
-brokenRhythm = dnext +++ dprev
+brokenRhythm :: Parser AbcBrokenRhythm
+brokenRhythm = dnext <|> dprev
   where dnext = AbcDottedRight <$> counting1 (char '<')
         dprev = AbcDottedLeft  <$> counting1 (char '>')
 
   
 
 
-tie :: ReadP AbcTie
+tie :: Parser AbcTie
 tie = char '-' >> return AbcTie
 
 
 
-gracings :: ReadP AbcGracing
-gracings = tilde <++ stacatto <++ downBow <++ upBow
+gracings :: Parser AbcGracing
+gracings = tilde <|> stacatto <|> downBow <|> upBow
   where tilde     = AbcTilde    <$ char '~'
         stacatto  = AbcStacatto <$ char '.'
         downBow   = AbcDownBow  <$ char 'v'
         upBow     = AbcUpDown   <$ char 'u'
 
 
-graceNotes :: ReadP Grace_notes  
+graceNotes :: Parser Grace_notes  
 graceNotes = braces $ many1 pitch
   where braces = between (char '{') (char '}') 
 
 
 
-guitarChord :: ReadP AbcGuitarChord
-guitarChord = fchord <++ unchord
+guitarChord :: Parser AbcGuitarChord
+guitarChord = fchord <|> unchord
   where 
     fchord  = dblQuoted formalChord
     unchord = AbcUninterpretedChord <$> dblQuoted text
 
 
-formalChord :: ReadP AbcGuitarChord
+formalChord :: Parser AbcGuitarChord
 formalChord = AbcFormalChord <$> basenote <*> pmaybe chordType 
                                           <*> pmaybe (char '/' >> basenote)
 
     
-chordType :: ReadP String
-chordType = longest $
+chordType :: Parser String
+chordType = choice $ map string
     ["m", "7", "m7", "0", "o", "+", "mb5", "sus", "sus4"
         , "maj7", "mmaj7", "7sus4", "dim", "dim7", "7b5", "m7b5"
         , "6", "b6", "m6", "mb6", "46", "maj9", "9", "add9"
@@ -374,8 +375,8 @@ chordType = longest $
 
 
 
-barline :: ReadP AbcBarline
-barline = choiceB [ barDbl, barThickThin, barThinThick,
+barline :: Parser AbcBarline
+barline = choice [ barDbl, barThickThin, barThinThick,
                     repLeft, repRight, repBoth, barSgl ]
   where barSgl       = AbcSingleBar     <$ char '|'
         barDbl       = AbcDoubleBar     <$ string "||" 
@@ -388,8 +389,8 @@ barline = choiceB [ barDbl, barThickThin, barThinThick,
 
 
   
-nthRepeat :: ReadP AbcRepeatMark 
-nthRepeat = choiceB [firstRep, secondRep, firstEnd, secondEnd]
+nthRepeat :: Parser AbcRepeatMark 
+nthRepeat = choice [firstRep, secondRep, firstEnd, secondEnd]
   where 
     firstRep     = AbcFirstRepeat  <$ string "[1"
     secondRep    = AbcSecondRepeat <$ string "[2"
@@ -398,24 +399,24 @@ nthRepeat = choiceB [firstRep, secondRep, firstEnd, secondEnd]
 
 
                   
-beginSlur, endSlur :: ReadP AbcSlur                
+beginSlur, endSlur :: Parser AbcSlur                
 beginSlur   = AbcBeginSlur <$ char '('
 endSlur     = AbcEndSlur   <$ char ')'
 
 
-midTuneField :: ReadP AbcField
+midTuneField :: Parser AbcField
 midTuneField = tuneField
 
 
-tuneField :: ReadP AbcField
-tuneField = choiceB [ fieldElemskip, fieldKey, fieldDefaultLength,
+tuneField :: Parser AbcField
+tuneField = choice [ fieldElemskip, fieldKey, fieldDefaultLength,
                       fieldMeter, fieldPart, fieldTempo, fieldTitle, 
                       fieldWords ]
 
 
 
 
-fieldPart, fieldWords :: ReadP AbcField
+fieldPart, fieldWords :: Parser AbcField
 fieldPart = readField 'P' partSpec AbcPartField
   
 fieldWords = readField 'W' ws AbcWordsField
@@ -424,30 +425,30 @@ fieldWords = readField 'W' ws AbcWordsField
 
 userDefined = satisfy $ (flip elem) ['H'..'Z']
 
-texCommand :: ReadP Tex_command
+texCommand :: Parser Tex_command
 texCommand = (char '\\') *> text <* linefeed
 
 
-space :: ReadP ()     
-space = () <$ (char ' ' +++ char '\t')
+space :: Parser ()     
+space = () <$ (char ' ' <|> char '\t')
 
 
-comment :: ReadP ()
+comment :: Parser ()
 comment = char '%' *> text *> ending
-  where ending = linefeed +++ noLineBreak +++ lineBreak
+  where ending = linefeed <|> noLineBreak <|> lineBreak
     
-lineBreak :: ReadP ()  
+lineBreak :: Parser ()  
 lineBreak = char '!' *> linefeed
     
-noLineBreak :: ReadP ()    
+noLineBreak :: Parser ()    
 noLineBreak = char '\\' *> linefeed
 
-linefeed :: ReadP ()     
+linefeed :: Parser ()     
 linefeed = () <$ choice [char '\r',  char '\n']
 
 
-text :: ReadP [Char]
-text = munch1 isTextChar
+text :: Parser [Char]
+text = many1 (satisfy isTextChar)
   where isTextChar :: Char -> Bool
         isTextChar c = isAlpha c || isDigit c || isSpecial c
         
@@ -464,44 +465,24 @@ text = munch1 isTextChar
 -- Useful combinators
 --------------------------------------------------------------------------------                  
 
--- returns the longest sequence of successful parsers, rather than their
--- permutations
-repeated :: ReadP a -> ReadP [a]
-repeated p = rep []
-  where rep acc = (p >>= \a -> rep (a:acc)) <++ return (reverse acc)
-  
-  
-choiceB :: [ReadP a] -> ReadP a
--- ^ Left-biased version of choice        
-choiceB []     = pfail
-choiceB [p]    = p
-choiceB (p:ps) = p <++ choiceB ps
-
-chooseStringB :: [String] -> ReadP String
-chooseStringB = choiceB . map string
 
 
-chooseString :: [String] -> ReadP String
 chooseString = choice . map string
 
-counting, counting1 :: ReadP a -> ReadP Int
+counting, counting1 :: Parser a -> Parser Int
 
-counting p = counting1 p <++ return 0
+counting p = length <$> many p
 
-counting1 p = counting' 1
-  where counting' i = p >> counting' (i+1) <++ return i
+counting1 p = length <$> many1 p
 
 dblQuoted = between (char '"') (char '"')
 
-longest :: [String] -> ReadP String 
-longest = chooseStringB . reverse . sortBy longstring
-  where longstring s1 s2 = compare (length s1) (length s2)
 
-readNat :: ReadP Int
-readNat =  munch1 isDigit >>= return . read 
+readNat :: Parser Int
+readNat =  many1 (satisfy isDigit) >>= return . read 
 
 
-pmaybe :: ReadP a -> ReadP (Maybe a)
+pmaybe :: Parser a -> Parser (Maybe a)
 pmaybe p = option Nothing (p >>= return . Just)
 
 parens = between (char '(') (char ')')
