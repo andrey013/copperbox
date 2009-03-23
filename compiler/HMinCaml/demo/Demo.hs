8f8389787c5b1597fe53b33d023456abefa20f73
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2009-03-23 14:46:39 +0000

    HMinCaml - added a pretty printer.

--- compiler/HMinCaml/demo/Demo.hs
+++ compiler/HMinCaml/demo/Demo.hs
@@ -4,15 +4,22 @@ module Demo where
 
 import HMinCaml.Main 
 import HMinCaml.Parser
+import HMinCaml.Pretty
 import HMinCaml.Typing
 
+import Text.PrettyPrint.Leijen  
 
 demo :: IO ()
 demo = do 
   ans <- parseMinCaml "ack.ml"
   case ans of 
     Left err -> putStrLn err
-    Right a -> print (infer a)
+    Right a -> putStrLn $ showWidth 80 (pretty a)
+    
+    
+
+showWidth :: Int -> Doc -> String
+showWidth w d = displayS (renderPretty 0.8 w d) ""
 
 
 
