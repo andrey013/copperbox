c540fb2e615d4c2dcaed256c52b2d6ba4977bd1c
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2009-05-18 14:15:43 +0000

    Wumpus - svg colours now literals in the range [0,1].

--- libs/graphics/Wumpus/src/Graphics/Wumpus/SVGColours.hs
+++ libs/graphics/Wumpus/src/Graphics/Wumpus/SVGColours.hs
@@ -170,451 +170,448 @@ import Graphics.Wumpus.Colour ( Colour3 )
 import Data.SG ( Triple(..) )
 
 import Prelude hiding ( tan )
-
-rgb :: Double -> Double -> Double -> Colour3
-rgb r g b = Triple (r / 255.0, g / 255.0, b / 255.0)
   
   
 aliceblue           :: Colour3
-aliceblue           = rgb 240 248 255
+aliceblue           = Triple (0.941, 0.973, 1.0)
 
 antiquewhite        :: Colour3
-antiquewhite        = rgb 250 235 215
+antiquewhite        = Triple (0.980, 0.922, 0.843)
 
 aqua                :: Colour3
-aqua                = rgb 0 255 255
+aqua                = Triple (0.0, 1.0, 1.0)
 
 aquamarine          :: Colour3
-aquamarine          = rgb 127 255 212
+aquamarine          = Triple (0.498, 1.0, 0.831)
 
 azure               :: Colour3
-azure               = rgb 240 255 255
+azure               = Triple (0.941, 1.0, 1.0)
 
 beige               :: Colour3  
-beige               = rgb 245 245 220
+beige               = Triple (0.961, 0.961, 0.863)
 
 bisque              :: Colour3
-bisque              = rgb 255 228 196
+bisque              = Triple (1.0, 0.894, 0.769)
 
 black               :: Colour3
-black               = rgb  0 0 0
+black               = Triple (0.0, 0.0, 0.0)
 
 blanchedalmond      :: Colour3
-blanchedalmond      = rgb 255 235 205
+blanchedalmond      = Triple (1.0, 0.922, 0.804)
 
 blue                :: Colour3
-blue                = rgb  0 0 255
+blue                = Triple (0.0, 0.0, 1.0)
 
 blueviolet          :: Colour3
-blueviolet          = rgb 138 43 226
+blueviolet          = Triple (0.541, 0.169, 0.886)
 
 brown               :: Colour3
-brown               = rgb 165 42 42
+brown               = Triple (0.647, 0.165, 0.165)
 
 burlywood           :: Colour3
-burlywood           = rgb 222 184 135
+burlywood           = Triple (0.871, 0.722, 0.529)
 
 cadetblue           :: Colour3
-cadetblue           = rgb  95 158 160
+cadetblue           = Triple (0.373, 0.620, 0.627)
 
 chartreuse          :: Colour3
-chartreuse          = rgb 127 255 0
+chartreuse          = Triple (0.498, 1.0, 0.0)
 
 chocolate           :: Colour3
-chocolate           = rgb 210 105 30
+chocolate           = Triple (0.824, 0.412, 0.118)
 
 coral               :: Colour3
-coral               = rgb 255 127 80
+coral               = Triple (1.0, 0.498, 0.314)
 
 cornflowerblue      :: Colour3
-cornflowerblue      = rgb 100 149 237
+cornflowerblue      = Triple (0.392, 0.584, 0.929)
 
 cornsilk            :: Colour3
-cornsilk            = rgb 255 248 220
+cornsilk            = Triple (1.0, 0.973, 0.863)
 
 crimson             :: Colour3
-crimson             = rgb 220 20 60
+crimson             = Triple (0.863, 0.078, 0.235)
 
 cyan                :: Colour3
-cyan                = rgb  0 255 255
+cyan                = Triple (0.0, 1.0, 1.0)
 
 darkblue            :: Colour3
-darkblue            = rgb  0 0 139
+darkblue            = Triple (0.0, 0.0, 0.545)
 
 darkcyan            :: Colour3
-darkcyan            = rgb  0 139 139
+darkcyan            = Triple (0.0, 0.545, 0.545)
 
 darkgoldenrod       :: Colour3
-darkgoldenrod       = rgb 184 134 11
+darkgoldenrod       = Triple (0.722, 0.545, 0.043)
 
 darkgray            :: Colour3
-darkgray            = rgb 169 169 169
+darkgray            = Triple (0.663, 0.663, 0.663)
 
 darkgreen           :: Colour3
-darkgreen           = rgb  0 100 0
+darkgreen           = Triple (0.0, 0.392, 0.0)
 
 darkgrey            :: Colour3
-darkgrey            = rgb 169 169 169
+darkgrey            = Triple (0.663, 0.663, 0.663)
 
 darkkhaki           :: Colour3
-darkkhaki           = rgb 189 183 107
+darkkhaki           = Triple (0.741, 0.718, 0.420)
 
 darkmagenta         :: Colour3
-darkmagenta         = rgb 139 0 139
+darkmagenta         = Triple (0.545, 0.0, 0.545)
 
 darkolivegreen      :: Colour3
-darkolivegreen      = rgb  85 107 47
+darkolivegreen      = Triple (0.333, 0.420, 0.184)
 
 darkorange          :: Colour3
-darkorange          = rgb 255 140 0
+darkorange          = Triple (1.0, 0.549, 0.0)
 
 darkorchid          :: Colour3
-darkorchid          = rgb 153 50 204
+darkorchid          = Triple (0.600, 0.196, 0.800)
 
 darkred             :: Colour3
-darkred             = rgb 139 0 0
+darkred             = Triple (0.545, 0.0, 0.0)
 
 darksalmon          :: Colour3
-darksalmon          = rgb 233 150 122
+darksalmon          = Triple (0.914, 0.588, 0.478)
 
 darkseagreen        :: Colour3
-darkseagreen        = rgb 143 188 143
+darkseagreen        = Triple (0.561, 0.737, 0.561)
 
 darkslateblue       :: Colour3
-darkslateblue       = rgb  72 61 139
+darkslateblue       = Triple (0.282, 0.239, 0.545)
 
 darkslategray       :: Colour3
-darkslategray       = rgb  47 79 79
+darkslategray       = Triple (0.184, 0.310, 0.310)
 
 darkslategrey       :: Colour3
-darkslategrey       = rgb  47 79 79
+darkslategrey       = Triple (0.184, 0.310, 0.310)
 
 darkturquoise       :: Colour3
-darkturquoise       = rgb  0 206 209
+darkturquoise       = Triple (0.0, 0.808, 0.820)
 
 darkviolet          :: Colour3
-darkviolet          = rgb 148 0 211
+darkviolet          = Triple (0.580, 0.0, 0.827)
 
 deeppink            :: Colour3
-deeppink            = rgb 255 20 147
+deeppink            = Triple (1.0, 0.078, 0.576)
 
 deepskyblue         :: Colour3
-deepskyblue         = rgb  0 191 255
+deepskyblue         = Triple (0.0, 0.750, 1.0)
 
 dimgray             :: Colour3
-dimgray             = rgb 105 105 105
+dimgray             = Triple (0.412, 0.412, 0.412)
 
 dimgrey             :: Colour3
-dimgrey             = rgb 105 105 105
+dimgrey             = Triple (0.412, 0.412, 0.412)
 
 dodgerblue          :: Colour3
-dodgerblue          = rgb  30 144 255
+dodgerblue          = Triple (0.118, 0.565, 1.0)
 
 firebrick           :: Colour3
-firebrick           = rgb 178 34 34
+firebrick           = Triple (0.698, 0.133, 0.133)
 
 floralwhite         :: Colour3
-floralwhite         = rgb 255 250 240
+floralwhite         = Triple (1.0, 0.980, 0.941)
 
 forestgreen         :: Colour3
-forestgreen         = rgb  34 139 34
+forestgreen         = Triple (0.133, 0.545, 0.133)
 
 fuchsia             :: Colour3
-fuchsia             = rgb 255 0 255
+fuchsia             = Triple (1.0, 0.0, 1.0)
 
 gainsboro           :: Colour3
-gainsboro           = rgb 220 220 220
+gainsboro           = Triple (0.863, 0.863, 0.863)
 
 ghostwhite          :: Colour3
-ghostwhite          = rgb 248 248 255
+ghostwhite          = Triple (0.973, 0.973, 1.0)
 
 gold                :: Colour3
-gold                = rgb 255 215 0
+gold                = Triple (1.0, 0.843, 0.0)
 
 goldenrod           :: Colour3
-goldenrod           = rgb 218 165 32
+goldenrod           = Triple (0.855, 0.647, 0.125)
 
 gray                :: Colour3
-gray                = rgb 128 128 128
+gray                = Triple (0.5, 0.5, 0.5)
 
 grey                :: Colour3
-grey                = rgb 128 128 128
+grey                = Triple (0.5, 0.5, 0.5)
 
 green               :: Colour3
-green               = rgb  0 128 0
+green               = Triple (0.0, 0.5, 0.0)
 
 greenyellow         :: Colour3
-greenyellow         = rgb 173 255 47
+greenyellow         = Triple (0.678, 1.0, 0.184)
 
 honeydew            :: Colour3
-honeydew            = rgb 240 255 240
+honeydew            = Triple (0.941, 1.0, 0.941)
 
 hotpink             :: Colour3
-hotpink             = rgb 255 105 180
+hotpink             = Triple (1.0, 0.412, 0.706)
 
 indianred           :: Colour3
-indianred           = rgb 205 92 92
+indianred           = Triple (0.804, 0.361, 0.361)
 
 indigo              :: Colour3
-indigo              = rgb  75 0 130
+indigo              = Triple (0.294, 0.0, 0.510)
 
 ivory               :: Colour3
-ivory               = rgb 255 255 240
+ivory               = Triple (1.0, 1.0, 0.941)
 
 khaki               :: Colour3
-khaki               = rgb 240 230 140
+khaki               = Triple (0.941, 0.902, 0.549)
 
 lavender            :: Colour3
-lavender            = rgb 230 230 250
+lavender            = Triple (0.902, 0.902, 0.980)
 
 lavenderblush       :: Colour3
-lavenderblush       = rgb 255 240 245
+lavenderblush       = Triple (1.0, 0.941, 0.961)
 
 lawngreen           :: Colour3
-lawngreen           = rgb 124 252 0
+lawngreen           = Triple (0.486, 0.988, 0.0)
 
 lemonchiffon        :: Colour3
-lemonchiffon        = rgb 255 250 205
+lemonchiffon        = Triple (1.0, 0.980, 0.804)
 
 lightblue           :: Colour3
-lightblue           = rgb 173 216 230
+lightblue           = Triple (0.678, 0.847, 0.902)
 
 lightcoral          :: Colour3
-lightcoral          = rgb 240 128 128
+lightcoral          = Triple (0.941, 0.5, 0.5)
 
 lightcyan           :: Colour3
-lightcyan           = rgb 224 255 255
+lightcyan           = Triple (0.878, 1.0, 1.0)
 
 lightgoldenrodyellow  :: Colour3
-lightgoldenrodyellow  = rgb 250 250 210
+lightgoldenrodyellow  = Triple (0.980, 0.980, 0.824)
 
 lightgray           :: Colour3
-lightgray           = rgb 211 211 211
+lightgray           = Triple (0.827, 0.827, 0.827)
 
 lightgreen          :: Colour3
-lightgreen          = rgb 144 238 144
+lightgreen          = Triple (0.565, 0.933, 0.565)
 
 lightgrey           :: Colour3
-lightgrey           = rgb 211 211 211
+lightgrey           = Triple (0.827, 0.827, 0.827)
 
 lightpink           :: Colour3
-lightpink           = rgb 255 182 193
+lightpink           = Triple (1.0, 0.714, 0.757)
 
 lightsalmon         :: Colour3
-lightsalmon         = rgb 255 160 122
+lightsalmon         = Triple (1.0, 0.627, 0.478)
 
 lightseagreen       :: Colour3
-lightseagreen       = rgb  32 178 170
+lightseagreen       = Triple (0.125, 0.698, 0.666)
 
 lightskyblue        :: Colour3
-lightskyblue        = rgb 135 206 250
+lightskyblue        = Triple (0.529, 0.808, 0.980)
 
 lightslategray      :: Colour3
-lightslategray      = rgb 119 136 153
+lightslategray      = Triple (0.466, 0.533, 0.600)
 
 lightslategrey      :: Colour3
-lightslategrey      = rgb 119 136 153
+lightslategrey      = Triple (0.466, 0.533, 0.600)
 
 lightsteelblue      :: Colour3
-lightsteelblue      = rgb 176 196 222
+lightsteelblue      = Triple (0.690, 0.769, 0.871)
 
 lightyellow         :: Colour3
-lightyellow         = rgb 255 255 224
+lightyellow         = Triple (1.0, 1.0, 0.878)
 
 lime                :: Colour3
-lime                = rgb  0 255 0
+lime                = Triple (0.0, 1.0, 0.0)
 
 limegreen           :: Colour3
-limegreen           = rgb  50 205 50
+limegreen           = Triple (0.196, 0.804, 0.196)
 
 linen               :: Colour3
-linen               = rgb 250 240 230
+linen               = Triple (0.980, 0.941, 0.902)
 
 magenta             :: Colour3
-magenta             = rgb 255 0 255
+magenta             = Triple (1.0, 0.0, 1.0)
 
 maroon              :: Colour3
-maroon              = rgb 128 0 0
+maroon              = Triple (0.5, 0.0, 0.0)
 
 mediumaquamarine    :: Colour3
-mediumaquamarine    = rgb 102 205 170
+mediumaquamarine    = Triple (0.4, 0.804, 0.666)
 
 mediumblue          :: Colour3
-mediumblue          = rgb 0 0 205
+mediumblue          = Triple (0.0, 0.0, 0.804)
 
 mediumorchid        :: Colour3
-mediumorchid        = rgb 186 85 211
+mediumorchid        = Triple (0.729, 0.333, 0.827)
 
 mediumpurple        :: Colour3
-mediumpurple        = rgb 147 112 219
+mediumpurple        = Triple (0.576, 0.439, 0.859)
 
 mediumseagreen      :: Colour3
-mediumseagreen      = rgb 60 179 113
+mediumseagreen      = Triple (0.235, 0.702, 0.443)
 
 mediumslateblue     :: Colour3
-mediumslateblue     = rgb 123 104 238
+mediumslateblue     = Triple (0.482, 0.408, 0.933)
 
 mediumspringgreen   :: Colour3
-mediumspringgreen   = rgb 0 250 154
+mediumspringgreen   = Triple (0.0, 0.980, 0.604)
 
 mediumturquoise     :: Colour3
-mediumturquoise     = rgb 72 209 204
+mediumturquoise     = Triple (0.282, 0.820, 0.800)
 
 mediumvioletred     :: Colour3
-mediumvioletred     = rgb 199 21 133
+mediumvioletred     = Triple (0.780, 0.082, 0.522)
 
 midnightblue        :: Colour3
-midnightblue        = rgb  25 25 112
+midnightblue        = Triple (0.098, 0.098, 0.439)
 
 mintcream           :: Colour3
-mintcream           = rgb 245 255 250
+mintcream           = Triple (0.961, 1.0, 0.980)
 
 mistyrose           :: Colour3
-mistyrose           = rgb 255 228 225
+mistyrose           = Triple (1.0, 0.894, 0.882)
 
 moccasin            :: Colour3
-moccasin            = rgb 255 228 181
+moccasin            = Triple (1.0, 0.894, 0.710)
 
 navajowhite         :: Colour3
-navajowhite         = rgb 255 222 173
+navajowhite         = Triple (1.0, 0.871, 0.678)
 
 navy                :: Colour3
-navy                = rgb  0 0 128
+navy                = Triple (0.0, 0.0, 0.5)
 
 oldlace             :: Colour3
-oldlace             = rgb 253 245 230
+oldlace             = Triple (0.992, 0.961, 0.902)
 
 olive               :: Colour3
-olive               = rgb 128 128 0
+olive               = Triple (0.5, 0.5, 0.0)
 
 olivedrab           :: Colour3
-olivedrab           = rgb 107 142 35
+olivedrab           = Triple (0.420, 0.557, 0.137)
 
 orange              :: Colour3
-orange              = rgb 255 165 0
+orange              = Triple (1.0, 0.647, 0.0)
 
 orangered           :: Colour3
-orangered           = rgb 255 69 0
+orangered           = Triple (1.0, 0.271, 0.0)
 
 orchid              :: Colour3
-orchid              = rgb 218 112 214
+orchid              = Triple (0.855, 0.439, 0.839)
 
 palegoldenrod       :: Colour3
-palegoldenrod       = rgb 238 232 170
+palegoldenrod       = Triple (0.933, 0.910, 0.666)
 
 palegreen           :: Colour3
-palegreen           = rgb 152 251 152
+palegreen           = Triple (0.596, 0.984, 0.596)
 
 paleturquoise       :: Colour3
-paleturquoise       = rgb 175 238 238
+paleturquoise       = Triple (0.686, 0.933, 0.933)
 
 palevioletred       :: Colour3
-palevioletred       = rgb 219 112 147
+palevioletred       = Triple (0.859, 0.439, 0.576)
 
 papayawhip          :: Colour3
-papayawhip          = rgb 255 239 213
+papayawhip          = Triple (1.0, 0.937, 0.835)
 
 peachpuff           :: Colour3
-peachpuff           = rgb 255 218 185
+peachpuff           = Triple (1.0, 0.855, 0.725)
 
 peru                :: Colour3
-peru                = rgb 205 133 63
+peru                = Triple (0.804, 0.522, 0.247)
 
 pink                :: Colour3
-pink                = rgb 255 192 203
+pink                = Triple (1.0, 0.753, 0.796)
 
 plum                :: Colour3
-plum                = rgb 221 160 221
+plum                = Triple (0.867, 0.627, 0.867)
 
 powderblue          :: Colour3
-powderblue          = rgb 176 224 230
+powderblue          = Triple (0.690, 0.878, 0.902)
 
 purple              :: Colour3
-purple              = rgb 128 0 128
+purple              = Triple (0.5, 0.0, 0.5)
 
 red                 :: Colour3
-red                 = rgb 255 0 0
+red                 = Triple (1.0, 0.0, 0.0)
 
 rosybrown           :: Colour3
-rosybrown           = rgb 188 143 143
+rosybrown           = Triple (0.737, 0.561, 0.561)
 
 royalblue           :: Colour3
-royalblue           = rgb  65 105 225
+royalblue           = Triple (0.255, 0.412, 0.882)
 
 saddlebrown         :: Colour3
-saddlebrown         = rgb 139 69 19
+saddlebrown         = Triple (0.545, 0.271, 0.075)
 
 salmon              :: Colour3
-salmon              = rgb 250 128 114
+salmon              = Triple (0.980, 0.5, 0.447)
 
 sandybrown          :: Colour3
-sandybrown          = rgb 244 164 96
+sandybrown          = Triple (0.957, 0.643, 0.376)
 
 seagreen            :: Colour3
-seagreen            = rgb  46 139 87
+seagreen            = Triple (0.180, 0.545, 0.341)
 
 seashell            :: Colour3
-seashell            = rgb 255 245 238
+seashell            = Triple (1.0, 0.961, 0.933)
 
 sienna              :: Colour3
-sienna              = rgb 160 82 45
+sienna              = Triple (0.627, 0.322, 0.176)
 
 silver              :: Colour3
-silver              = rgb 192 192 192
+silver              = Triple (0.753, 0.753, 0.753)
 
 skyblue             :: Colour3
-skyblue             = rgb 135 206 235
+skyblue             = Triple (0.529, 0.808, 0.922)
 
 slateblue           :: Colour3
-slateblue           = rgb 106 90 205
+slateblue           = Triple (0.416, 0.353, 0.804)
 
 slategray           :: Colour3
-slategray           = rgb 112 128 144
+slategray           = Triple (0.439, 0.5, 0.565)
 
 slategrey           :: Colour3
-slategrey           = rgb 112 128 144
+slategrey           = Triple (0.439, 0.5, 0.565)
 
 snow                :: Colour3
-snow                = rgb 255 250 250
+snow                = Triple (1.0, 0.980, 0.980)
 
 springgreen         :: Colour3
-springgreen         = rgb  0 255 127
+springgreen         = Triple (0.0, 1.0, 0.498)
 
 steelblue           :: Colour3
-steelblue           = rgb  70 130 180
+steelblue           = Triple (0.275, 0.510, 0.706)
 
 tan                 :: Colour3
-tan                 = rgb 210 180 140
+tan                 = Triple (0.824, 0.706, 0.549)
 
 teal                :: Colour3
-teal                = rgb  0 128 128
+teal                = Triple (0.0, 0.5, 0.5)
 
 thistle             :: Colour3
-thistle             = rgb 216 191 216
+thistle             = Triple (0.847, 0.750, 0.847)
 
 tomato              :: Colour3
-tomato              = rgb 255 99 71
+tomato              = Triple (1.0, 0.388, 0.278)
 
 turquoise           :: Colour3
-turquoise           = rgb  64 224 208
+turquoise           = Triple (0.251, 0.878, 0.816)
 
 violet              :: Colour3
-violet              = rgb 238 130 238
+violet              = Triple (0.933, 0.510, 0.933)
 
 wheat               :: Colour3
-wheat               = rgb 245 222 179
+wheat               = Triple (0.961, 0.871, 0.702)
 
 white               :: Colour3
-white               = rgb 255 255 255
+white               = Triple (1.0, 1.0, 1.0)
 
 whitesmoke          :: Colour3
-whitesmoke          = rgb 245 245 245
+whitesmoke          = Triple (0.961, 0.961, 0.961)
 
 yellow              :: Colour3
-yellow              = rgb 255 255 0
+yellow              = Triple (1.0, 1.0, 0.0)
 
 yellowgreen         :: Colour3
-yellowgreen         = rgb 154 205 50
+yellowgreen         = Triple (0.604, 0.804, 0.196)
 
 
 
