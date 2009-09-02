eb8e907f3f17b9255a46c000e89774bff437ee72
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2009-09-02 10:56:14 +0000

    Bala2 - work on chord construction.

--- bala/Bala2/src/Bala/Chord.hs
+++ bala/Bala2/src/Bala/Chord.hs
@@ -23,6 +23,7 @@ import Data.AffineSpace
 
 import Data.IntMap ( IntMap )
 import qualified Data.IntMap as IM
+import Data.Monoid
 
 data Chord = Chord { chordRoot :: Pitch, chordIntervals :: IntMap Int }
   deriving (Eq)
@@ -43,18 +44,110 @@ extractIntervals = map (uncurry makeInterval) . IM.toAscList . chordIntervals
 chordPitches :: Chord -> [Pitch]
 chordPitches ch@(Chord p _) = map (p .+^) $ extractIntervals ch
 
-major :: Pitch -> Chord
-major p = Chord p $ IM.fromList [(1,0), (3,4), (5,7)]
 
+
+literalForm :: Pitch -> [Interval] -> Chord
+literalForm p ivals = Chord p $ IM.fromList $ map intervalPair ivals
+
+stepForm :: Pitch -> [Interval] -> Chord
+stepForm p ivals = Chord p im where
+  im = IM.fromList $ map intervalPair $ scanl mappend mempty ivals 
+
+
+major :: Pitch -> Chord
+major = literalForm `flip` [perfect1, major3, perfect5]
 
 minor :: Pitch -> Chord
-minor p = Chord p $ IM.fromList [(1,0), (3,3), (5,7)]
+minor = literalForm `flip` [perfect1, minor3, perfect5]
+
+diminished :: Pitch -> Chord
+diminished = stepForm `flip` [minor3, minor3]
+
+augmented :: Pitch -> Chord
+augmented = stepForm `flip` [major3,major3]
+
 
 
 immap :: (IntMap Int -> IntMap Int) -> Chord -> Chord
 immap f (Chord p ivls) = Chord p (f ivls)
 
+insert :: Interval -> Chord -> Chord
+insert ival = immap (uncurry IM.insert $ intervalPair ival)
 
+delete :: Int -> Chord -> Chord
+delete i = immap (IM.delete i)
+
+-- The 'Pachet' vocabulary - see Francois Pachet:
+-- /An Object-Oriented Representation of Pitch-Classes, Intervals,
+-- Scales and Chords: The basic MusES/
+
+noRoot :: Chord -> Chord
+noRoot = delete 1
+
+no3 :: Chord -> Chord
+no3 = delete 3
+
+no5 :: Chord -> Chord
+no5 = delete 5
+
+no7 :: Chord -> Chord
+no7 = delete 7
+
+no9 :: Chord -> Chord
+no9 = delete 9
+
+no11 :: Chord -> Chord
+no11 = delete 11
+
+no13 :: Chord -> Chord
+no13 = delete 13
+
+
+min7 :: Chord -> Chord
+min7 = insert minor7
 
 maj7 :: Chord -> Chord
-maj7 = immap (uncurry IM.insert $ intervalPair major7)  
+maj7 = insert major7
+
+dim7 :: Chord -> Chord
+dim7 = insert diminished7
+
+
+sus4 :: Chord -> Chord
+sus4 = insert perfect4 . delete 3
+
+-- dim7 :: Chord -> Chord
+-- dim7 = stepForm [perfect1, minor3, diminished5, diminished7]
+
+dim9 :: Chord -> Chord
+dim9 = insert diminished9 . insert minor7
+
+maj9 :: Chord -> Chord
+maj9 = insert major9 . maj7 
+
+min9 :: Chord -> Chord
+min9 = insert major9 . min7 
+
+aug9 :: Chord -> Chord
+aug9 = insert augmented9 . insert minor7
+
+
+dim11 :: Chord -> Chord
+dim11 = insert diminished11 . dim9
+
+min11 :: Chord -> Chord
+min11 = insert perfect11 . min9 
+
+aug11 :: Chord -> Chord
+aug11 = insert augmented11 . aug9
+
+
+dim13 :: Chord -> Chord
+dim13 = insert diminished13 . dim11
+
+
+-- maj13 :: Chord -> Chord
+-- maj13 = insert major9 . maj9
+
+aug13 :: Chord -> Chord
+aug13 = insert augmented13 . aug11
--- bala/Bala2/src/Bala/Interval.hs
+++ bala/Bala2/src/Bala/Interval.hs
@@ -129,34 +129,82 @@ intervalPair (Interval i j) = (fromRI i,j)
 
 
 -- 2 3 6 7
-major2 :: Interval
-major3 :: Interval
-major6 :: Interval
-major7 :: Interval
-major2 = makeInterval 2 2
-major3 = makeInterval 3 4
-major6 = makeInterval 6 9
-major7 = makeInterval 7 11
+major2              :: Interval
+major3              :: Interval
+major6              :: Interval
+major7              :: Interval
+major9              :: Interval
+major2              = makeInterval 2 2
+major3              = makeInterval 3 4
+major6              = makeInterval 6 9
+major7              = makeInterval 7 11
+major9              = perfect8 `mappend` major2
+
+minor2              :: Interval
+minor3              :: Interval
+minor6              :: Interval
+minor7              :: Interval
+minor2              = makeInterval 2 1
+minor3              = makeInterval 3 3
+minor6              = makeInterval 6 8
+minor7              = makeInterval 7 10
 
 
-minor2 :: Interval
-minor3 :: Interval
-minor6 :: Interval
-minor7 :: Interval
-minor2 = makeInterval 2 1
-minor3 = makeInterval 3 3
-minor6 = makeInterval 6 8
-minor7 = makeInterval 7 10
+perfect1            :: Interval 
+perfect4            :: Interval 
+perfect5            :: Interval 
+perfect8            :: Interval 
+perfect11           :: Interval
+perfect1            = makeInterval 1 0
+perfect4            = makeInterval 4 5
+perfect5            = makeInterval 5 7
+perfect8            = makeInterval 8 12
+perfect11           = perfect8 `mappend` perfect4
+
+diminished2         :: Interval
+diminished3         :: Interval
+diminished4         :: Interval
+diminished5         :: Interval
+diminished6         :: Interval
+diminished7         :: Interval
+diminished8         :: Interval
+diminished9         :: Interval
+diminished11        :: Interval
+diminished13        :: Interval
+diminished2         = makeInterval 2 0
+diminished3         = makeInterval 3 2
+diminished4         = makeInterval 4 4
+diminished5         = makeInterval 5 6
+diminished6         = makeInterval 6 7
+diminished7         = makeInterval 7 9
+diminished8         = makeInterval 8 11
+diminished9         = perfect8 `mappend` diminished2
+diminished11        = perfect8 `mappend` diminished4 
+diminished13        = perfect8 `mappend` diminished6 
 
 
-perfect1 :: Interval 
-perfect4 :: Interval 
-perfect5 :: Interval 
-perfect8 :: Interval 
-perfect1 = makeInterval 1 0
-perfect4 = makeInterval 4 5
-perfect5 = makeInterval 5 7
-perfect8 = makeInterval 8 12
+augmented2          :: Interval
+augmented3          :: Interval
+augmented4          :: Interval
+augmented5          :: Interval
+augmented6          :: Interval
+augmented7          :: Interval
+augmented8          :: Interval
+augmented9          :: Interval
+augmented11         :: Interval
+augmented13         :: Interval
+augmented2          = makeInterval 2 3
+augmented3          = makeInterval 3 5
+augmented4          = makeInterval 4 6
+augmented5          = makeInterval 5 8
+augmented6          = makeInterval 6 10
+augmented7          = makeInterval 7 12
+augmented8          = makeInterval 8 13
+augmented9          = perfect8 `mappend` augmented2
+augmented11         = perfect8 `mappend` augmented4 
+augmented13         = perfect8 `mappend` augmented6 
+
+
 
 
 -- amod7 [1-7]
