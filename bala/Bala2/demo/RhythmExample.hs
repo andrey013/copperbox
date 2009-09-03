cc629b2c2498a4205bf9cc2b6ed1515a924a6eda
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2009-09-03 20:18:54 +0000

    Bala2 - some work on rhythm patterns and pulsations.

--- bala/Bala2/demo/RhythmExample.hs
+++ bala/Bala2/demo/RhythmExample.hs
@@ -9,46 +9,38 @@ import Bala.RhythmPattern
 
 import Mullein.LilyPond hiding ( Pulse )
 
+import Data.Stream ( Stream )
 import qualified Data.Stream              as WS
+import Data.Stream.Hinze.Stream ( (<:) )
 import qualified Data.Stream.Hinze.Stream as HS
 import Text.PrettyPrint.Leijen
 
-
+import Data.Ratio
 import qualified Data.Set                 as Set
 
+afoxe_upper = makeSubsetPattern 8 [2,4,6,8]
+afoxe_lower = makeSubsetPattern 8 [5,7]
 
-demo0 = showBox dus
+-- Hmmm, we're not actually using the pulsation pattern here!
 
-dus :: RhythmPattern
-dus = makeRhythmPattern s 1 where
-  s = [N 2,N 2,R 1,N 2,N 1, R 1,N 2,N 1,R 2,N 2]
+afoxeUs :: Stream PDGlyph
+afoxeUs = unwind phi 0 $ pulse afoxe_upper where
+  phi _ 0 = (Left snr, 1)
+  phi _ 1 = (Left (c 6 en), 2)
+  phi _ 2 = (Right [c 6 sn, snr], 1)
 
-afoxe_upper_rhythm :: RhythmPattern
-afoxe_upper_rhythm = makeRhythmPattern s 4 where
-  s =  [R 1, N 2,N 1]
+-- Mullien - puts tie at start of new bar rather than end of 
+-- old bar...
 
+afoxeLs :: Stream PDGlyph
+afoxeLs = unwind phi 0 $ pulse afoxe_lower where
+  phi _ 0 = (Left qnr, 1)
+  phi _ 1 = (Left (c 5 en), 2)
+  phi _ 2 = (Right [c 5 en, {- Tie, -} c 5 qn], 1)
 
--- Not quite right - first bar is a rest subsequent bars are 
--- tied...
-afoxe_lower_rhythm :: RhythmPattern
-afoxe_lower_rhythm = makeRhythmPattern s 4 where
-  s =  [R 4, N 2,N 2]
-
-
-
-afoxeU :: [PDGlyph]
-afoxeU = fmap fn $ takeNBars 4 8 $ toStream afoxe_upper_rhythm
-  where
-   fn (R 1) = snr
-   fn (N 1) = c 6 sn
-   fn (N 2) = c 6 en
-
-
-afoxeL :: [PDGlyph]
-afoxeL = fmap fn $ takeNBars 4 8 $ toStream afoxe_lower_rhythm
-  where
-   fn (R 4) = qnr
-   fn (N 2) = c 5 en
+afoxeU = WS.take (4*6) afoxeUs
+afoxeL = WS.take (3+3*4) afoxeLs
+ 
 
 
 demo1 :: Doc
@@ -65,4 +57,18 @@ output1 =  writeDoc "afoxe.ly"  demo1
 
 twoFourTime :: MeterPattern
 twoFourTime = makeMeterPattern 2 4
+
+
+
+-- rest, note, note, note, note
+
+z1 = WS.take 10 $ fmap (%16) $ pulse afoxe_upper 
+
+
+z2 = WS.take 10 $ metricalPartition 8 $ pulse afoxe_lower
+z2' = WS.take 10 $ metricalPartition' 8 $ pulse afoxe_lower
+
+-- z3 = WS.take 20 $ pass 1 [idS, split, idS] () $ pulse afoxe_upper
+
+
 
--- bala/Bala2/src/Bala/RhythmPattern.hs
+++ bala/Bala2/src/Bala/RhythmPattern.hs
@@ -14,67 +14,100 @@
 --
 --------------------------------------------------------------------------------
 
-module Bala.RhythmPattern  where
+module Bala.RhythmPattern where
 
-import Data.Stream.Hinze.Stream 
 
--- import Data.Set ( Set )
--- import qualified Data.Set as Set
+import Data.Stream ( Stream, (<:>), head, tail )
+import qualified Data.Stream as S
+
+import Data.Stream.Hinze.Stream ( (<<) )
+import qualified Data.Stream.Hinze.Stream as HS
+
+
+import Data.List ( unfoldr )
+import Data.Set ( Set )
+import qualified Data.Set as Set
 
 import Prelude hiding (head, tail)
 import qualified Prelude as Pre
 
--- Rhythmic Pulse
-data Pulse = N Int | R Int
+
+
+-- | Represent rhythm patterns in /subset notation/. Subset 
+-- notation is useful as it readily accommodates rhythms that 
+-- don't start on a beat.
+data SubsetPattern = SubsetPattern { 
+      timespan :: Int,
+      pulseSet :: (Set Int)
+    }
   deriving (Eq,Show)
 
--- Represent rhythm patterns as a list of pulses. This is more 
--- useful than say subset patterns or box notation, because it
--- contains more information (length of the pulses - rather than
--- just their onset).
-newtype RhythmPattern = RhythmPattern { getPulseList :: [Pulse] }
 
-makeRhythmPattern :: [Pulse] -> Int -> RhythmPattern
-makeRhythmPattern xs 1 = RhythmPattern xs
-makeRhythmPattern xs n 
-    | n > 1            = RhythmPattern $ concat (replicate n xs)
-    | otherwise        = error $ 
-          "makeRhythmPattern - cannot make RhythmPattern with " 
-          ++ show n ++ " repetitions."
-                         
-                          
-
-
-timespan :: RhythmPattern -> Int
-timespan = foldr fn 0 . getPulseList where
-  fn (N i) n = n+i
-  fn (R i) n = n+i
-
-
-
--- | Does the rhythm pattern start on an on-beat?
-onBeat :: RhythmPattern -> Bool
-onBeat (RhythmPattern ((N _):_)) = True
-onBeat _                         = False
+-- | Build a SubsetPattern. This function throws a if the subset 
+-- list is empty, ot if it it contains indexes outside the range 
+-- [0..n-1].
+makeSubsetPattern :: Int -> [Int] -> SubsetPattern
+makeSubsetPattern _ [] = error "makeSubsetPattern - empty list"
+makeSubsetPattern n xs 
+    | all (>=0) xs && n >= maximum xs 
+                        = SubsetPattern n (Set.fromList xs)
+    | otherwise         = error "makeSubsetPattern - invalid data"
 
 
 
 -- | Print a SubsetPattern in /box notation/.
-showBox :: RhythmPattern -> String
-showBox (RhythmPattern xs) = foldr fn "" xs
-  where
-    fn (N i) acc = 'X' : replicate (i-1) '.' ++ acc
-    fn (R i) acc = replicate i '.' ++ acc
+showBox :: SubsetPattern -> String
+showBox (SubsetPattern t st) = unfoldr phi 1 where
+  phi n | n > t             = Nothing
+  phi n | n `Set.member` st = Just ('X',n+1)
+        | otherwise         = Just ('.',n+1)
+
+-- cannot use @diff@ in Hinze.Stream as repeating the subset 
+-- pattern generates a /numerical reset/ in the stream.
+pulse :: SubsetPattern -> Stream Int
+pulse (SubsetPattern t st) = mydiff strm where
+  strm      = (Set.toList st) << strm  
+  mydiff s  = S.zipWith op s (1 <:> s)
+
+  op a b | a > b        = a - b
+         | otherwise    = (a+t) - b
 
 
-toStream :: RhythmPattern -> Stream Pulse
-toStream (RhythmPattern xs) = st where st = xs << st
+data E a = E a | Tied a 
+  deriving (Eq,Show)
+
+metricalPartition :: (Num a, Ord a) => a -> Stream a -> Stream (E a)
+metricalPartition sz str = step 0 (head str) (tail str) where 
+  step i hd rest 
+    | i+hd == sz = (E hd) <:> step 0 (head rest) (tail rest)  
+    | i+hd >  sz = (E $ sz-i ) <:> (Tied $ i+hd-sz)
+                               <:> step (i+hd-sz) (head rest) (tail rest)
+    | otherwise  = (E hd) <:> step (i+hd) (head rest) (tail rest)
 
 
-takeNBars :: Int -> Int -> Stream Pulse -> [Pulse]
-takeNBars n bar_len = step (n*bar_len) where
-  step a s = let sh = head s
-                 sz = case sh of (N i) -> i; (R i) -> i
-             in if (sz>a) then []
-                          else sh : step (a-sz) (tail s)
+metricalPartition' :: (Num a, Ord a) => a -> Stream a -> Stream (E a)
+metricalPartition' sz = unwind fn 0 where
+  fn a i | a+i == sz = (Left (E a),0)
+         | a+i > sz  = (Right [E $ sz-i, Tied $ i+a-sz], i+a-sz)
+         | otherwise = (Left (E a),a+i)
 
+
+
+
+
+-- buffered spigot
+bspigot :: (st -> Maybe ([b], st)) -> (st -> a -> st) -> st -> Stream a -> Stream b
+bspigot prod cons st s = case prod st of
+    Just (bs,st') -> bs << bspigot prod cons st' s
+    Nothing       -> bspigot prod cons (cons st (head s)) (tail s)
+
+
+-- this is really an unfoldMap but it can produce 1 or more 
+-- results at each step (actually it can produce zero - Right []).
+
+unwind :: (a -> st -> (Either b [b],st)) -> st -> Stream a -> Stream b
+unwind f st s = case f (head s) st of
+  (Left x,st')   -> x <:> unwind f st' (tail s)
+  (Right xs,st') -> xs << unwind f st' (tail s)
+
+ 
