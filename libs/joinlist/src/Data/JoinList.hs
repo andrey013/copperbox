a0d3b6dae01f9e40815bdf20d274158f8dfea93e
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2009-09-22 11:03:01 +0000

    JoinList - added haddock docs and packaged up with cabal.

--- libs/joinlist/src/Data/JoinList.hs
+++ libs/joinlist/src/Data/JoinList.hs
@@ -3,7 +3,7 @@
 --------------------------------------------------------------------------------
 -- |
 -- Module      :  Data.JoinList
--- Copyright   :  (c) Stephen Tetley 2008
+-- Copyright   :  (c) Stephen Tetley 2009
 -- License     :  BSD3
 --
 -- Maintainer  :  Stephen Tetley <stephen.tetley@gmail.com>
@@ -12,38 +12,60 @@
 --
 -- A Join List datatype and operations. 
 --
--- Join Lists are symmetric lists, unlike the standard list 
--- datatype which is a cons list and builds by prefixing. 
+-- Join Lists are symmetric lists where joining two lists 
+-- (catenation, aka (++)) is a cheap operation. This constrasts 
+-- with the regular list datatype which is a cons list: while 
+-- consing on a regular list (building a list by adding an 
+-- elementary prefix) is by nature cheap, joining (++) is 
+-- expensive. 
 --
 --------------------------------------------------------------------------------
 
 
 module Data.JoinList 
   ( 
+  -- * Join list datatype
     JoinList
 
+  -- * Elementary construction  
   , empty
-  , null
   , wrap
   , ( ++ )
+  , join
+
+  -- * Basic functions  
+  , null
   , concat
+  , length
+  , map
+
+  -- * Building join lists
   , replicate
   , repeated
+
+  -- * Generalized fold
   , gfold
 
+  -- * Zipping
+  , xzip
+  , xzipWith
+
+  -- * Conversion between join lists and regular lists
   , toList
   , fromList
+  
   ) where
 
 
 import Control.Applicative hiding ( empty )
 import Data.Monoid
 import Data.Traversable
+import Text.Show ( showListWith )
 
 import qualified Data.Foldable as F
 import qualified Data.List     as L
 
-import Prelude hiding ( (++), null, head, tail, concat, replicate )
+import Prelude hiding ( (++), null, length, map, concat, replicate )
 
 
 data JoinList a = Empty 
@@ -53,7 +75,7 @@ data JoinList a = Empty
 
 -- Bananaize !
 instance Show a => Show (JoinList a) where
-  showsPrec p xs = showChar '(' . showsPrec p (toList xs) . showChar ')'
+  showsPrec _ xs = showChar '(' . showListWith shows (toList xs) . showChar ')'
 
 instance Monoid (JoinList a) where
   mempty  = Empty
@@ -83,29 +105,61 @@ instance Traversable JoinList where
   traverse f (Single a) = Single <$> f a
   traverse f (Join t u) = Join <$> traverse f t <*> traverse f u
 
+--------------------------------------------------------------------------------
+-- Construction
 
+-- | Create an empty join list. 
 empty :: JoinList a
 empty = Empty
 
-null :: JoinList a -> Bool
-null Empty = True
-null _     = False
-
-
+-- | Create a singleton join list.
 wrap :: a -> JoinList a
 wrap = Single
 
+
 infixr 5 ++
 
+-- | Catenate two join lists. Unlike (++) on regular lists, 
+-- catenation on join lists is (relatively) cheap hence the 
+-- monicker /join list/.
 (++) :: JoinList a -> JoinList a -> JoinList a
 Empty ++ ys    = ys
 xs    ++ Empty = xs
 xs    ++ ys    = Join xs ys
 
+
+infixr 5 `join`
+
+-- | An alias for (++) that does not cause a name clash with the 
+-- Prelude.
+join :: JoinList a -> JoinList a -> JoinList a
+join = (++)
+
+
+--------------------------------------------------------------------------------
+-- Basic functions
+
+-- | Test whether a join list is empty.
+null :: JoinList a -> Bool
+null Empty = True
+null _     = False
+
+-- | Concatenate a (join) list of (join) lists. 
 concat :: JoinList (JoinList a) -> JoinList a
 concat = F.foldl' mappend mempty 
 
+-- | Get the length of a join list.
+length :: JoinList a -> Int
+length = gfold 0 (const 1) (+)
 
+-- | Map a function over a join list.
+map :: (a -> b) -> JoinList a -> JoinList b
+map = fmap
+
+--------------------------------------------------------------------------------
+-- Building join lists
+
+-- | Build a join list of n elements. 
 replicate :: Int -> a -> JoinList a
 replicate n a | n > 0     = step (n-1) (Single a)
               | otherwise = Empty
@@ -113,6 +167,7 @@ replicate n a | n > 0     = step (n-1) (Single a)
     step 0 xs = xs
     step i xs = step (i-1) $ Single a `Join` xs
 
+-- | Repeatedly build a join list by catenating the seed list.
 repeated :: Int -> JoinList a -> JoinList a
 repeated n xs | n > 0     = step (n-1) xs
               | otherwise = Empty
@@ -120,8 +175,10 @@ repeated n xs | n > 0     = step (n-1) xs
     step 0 ys = ys
     step i ys = step (i-1) $ xs `Join` ys
 
+--------------------------------------------------------------------------------
+-- Generalized fold
 
--- Jeremy Gibbons style general fold where each constructor has
+-- | A Jeremy Gibbons style generalised fold, where each constructor has
 -- an operation.
 gfold :: b                              -- param e, replaces Empty
       -> (a -> b)                       -- param f, replaces Single
@@ -133,10 +190,34 @@ gfold _ f _ (Single a) = f a
 gfold e f g (Join t u) = g (gfold e f g t) (gfold e f g u)
 
 
+--------------------------------------------------------------------------------
+-- Zipping
 
+-- | /cross zip/ - zip a join list against a regular list, 
+-- maintaining the shape of the join list provided the lengths 
+-- of the lists match.
+xzip :: JoinList a -> [b] -> JoinList (a,b)
+xzip = xzipWith (,)
+
+-- | Generalized cross zip - c.f. zipWith on regular lists.
+xzipWith :: (a -> b -> c) -> JoinList a -> [b] -> JoinList c
+xzipWith fn xs0 ys0       = fst $ step xs0 ys0
+  where 
+   step Empty      xs     = (Empty,xs)
+   step (Single a) (x:xs) = (Single (fn a x),xs)
+   step (Single _) []     = (Empty,[])
+   step (Join t u) xs     = (Join t' u',xs'') where
+                              (t',xs')  = step t xs
+                              (u',xs'') = step u xs'
+
+--------------------------------------------------------------------------------
+-- Conversion
+
+-- | Convert a join list to a regular list.
 toList :: JoinList a -> [a]
 toList = F.foldr (:) []
 
+-- | Build a join list from a regular list.
 fromList :: [a] -> JoinList a
 fromList []     = Empty
 fromList (x:xs) = L.foldl' (\a e -> a `Join` (Single e)) (Single x) xs
