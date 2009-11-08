a81ef9114dee274724abc38492e19ba75d0da8ad
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2009-11-08 21:55:18 +0000

    Wumpus - I've tidied up the interface provided but Core.Picture - i.e. removed the _bad_ label functions and implemented more instances for the drawing classes Fill, Ellipse etc.

--- libs/wumpus-core/demo/LabelPic.hs
+++ libs/wumpus-core/demo/LabelPic.hs
@@ -1,9 +1,53 @@
+{-# LANGUAGE ScopedTypeVariables        #-}
 
 module LabelPic where
 
 import Wumpus.Core
 
 import Data.FunctionExtras ( (#) )
+
+import Data.AffineSpace
+
+import Data.List                ( mapAccumR )
+
+--------------------------------------------------------------------------------
+
+
+  
+-- The functions here were in Wumpus.Core but they aren't very 
+-- good. They need to supplanted with something better.
+
+-- Firefox seems to have a issues with Courier.
+
+labelDefault :: LabelProps
+labelDefault = (psBlack, FontAttr "Courier" "Courier New" 10)
+
+frameDefault :: Num u => Frame2 u 
+frameDefault = ortho zeroPt
+
+psBlack :: PSColour
+psBlack = PSRgb 0 0 0
+
+
+-- The width guesses by picLabel1 and picLabel are very poor...
+
+picLabel1 :: (Num u, Ord u) => Int -> String -> Picture u
+picLabel1 fontsz str = Single (frameDefault, bb) lbl where
+  bb  = BBox zeroPt (P2 w (fromIntegral fontsz))
+  w   = fromIntegral $ fontsz * length str
+  lbl = Label1 labelDefault (Label zeroPt str) 
+
+
+picLabel :: forall u. (Num u, Ord u) => Int -> Int -> String -> Picture u
+picLabel fontsz linespace str = Multi (frameDefault, bb) lbls where
+  xs   = lines str
+  lc   = length xs
+  w    = fromIntegral $ fontsz * (maximum . map length) xs
+  h    = fromIntegral $ fontsz * lc + linespace*(lc-1)
+  bb   = BBox zeroPt (P2 w h)
+  lbls = snd $ mapAccumR fn zeroPt xs
+  fn pt ss = let pt' = pt .+^ (V2 (0::u) (fromIntegral $ fontsz + linespace))
+             in (pt', Label1 labelDefault (Label pt ss))
 
 
 
@@ -12,7 +56,7 @@ drawBounds Empty = Empty
 drawBounds p     = p `composite` (frame $ cstroke () ph) where
     ph   = vertexPath $ corners $ boundary p
 
-
+--------------------------------------------------------------------------------
 
 lbl1 :: Picture Double
 lbl1 = picLabel 10 3 "Hello\nWorld" {- # setRGBColour aquamarine4 
