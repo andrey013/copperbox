931101b83d1ff4cf72db119e2b9951bf1939a594
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2010-05-03 12:23:09 +0000

    utt - LilyPond output via Neume. Not satisfactory though... see blog.

--- bala/utt/demo/UseNeume.hs
+++ bala/utt/demo/UseNeume.hs
@@ -6,11 +6,11 @@ module UseNeume where
 
 import UTT.Base hiding ( relative )
 import UTT.Neume
-import UTT.TCBase
 
 import Neume.Core.Duration
 import Neume.Core.LilyPondFormat
 import Neume.Core.Pitch
+import Neume.Core.SpellingMap
 import Neume.Core.SyntaxGlyph
 import Neume.Core.SyntaxNoteList
 import Neume.Core.SyntaxScore
@@ -30,17 +30,22 @@ main =
   system   "lilypond triads.ly"             >>
   return ()
 
+-- Note using absolute score - Neume has something wrong with 
+-- chord rendering in relative mode...
+--
 
 ly_score :: Doc
 ly_score =  version "2.12.2" 
-        <$> scoreExpr (relative middle_c $ key c_nat "major" 
-                       <$> time 4 4
-                       <$> tune1)
+        <$> variableDef "melody" (nestBraces $ key c_nat "major" 
+                                            <$> time 4 4
+                                            <$> tune1)
+        <$> scoreExpr (variableUse "melody")
+
   where
-    tune1    = renderLyRelative ofmt rwspec triad_score
+    tune1    = renderLyAbsolute ofmt rwspec triad_score
     
     ofmt     = Ly_std_format_config       strip
-    rwspec   = Ly_relative_rewrite_config middle_c four_four_time strip
+    rwspec   = Ly_absolute_rewrite_config (-3) four_four_time strip
 
 
 triad_score :: Score (TLinear :. Z) (NoteList StdGlyph)
@@ -48,11 +53,22 @@ triad_score = fmap simpleNoteList $
     Linear ("a", triads_list) $ Nil
  
 triads_list :: [StdGlyph]
-triads_list = []
+triads_list = map chord3 $ upwards $ render1 key_c_maj progression
 
 
-triad :: (Pitch,Pitch,Pitch) -> Glyph () Pitch Duration
-triad (root,trd,fth) = chord [note root, note trd, note fth] dWhole False
+key_c_maj :: SpellingMap
+key_c_maj = makeSpellingMap 0 
+
+
+render1 :: SpellingMap -> [Triad] -> [Pitch3]
+render1 sm = map (pitches sm)
+
+
+progression :: [Triad]
+progression = take 4 $ iterate (`act` mediant) (Triad 0 Pos)
+
+chord3 :: Pitch3 -> Glyph () Pitch Duration
+chord3 (P3 root trd fth) = chord [note root, note trd, note fth] dWhole False
   where
-    note pch = Note () pch
+    note pch        = Note () pch
 
--- bala/utt/src/UTT/Neume.hs
+++ bala/utt/src/UTT/Neume.hs
@@ -23,19 +23,49 @@ import UTT.Base
 import UTT.Z12
 
 import Neume.Core.Pitch
+import Neume.Core.SpellingMap
+
+data Pitch3 = P3 Pitch Pitch Pitch
+  deriving (Eq,Show)
+
+-- This representation is not very good - it would
+-- be much better to have arithmetic...
+
+upInt :: [Int] -> [Int]
+upInt = mapWithPrevAns up
+  where
+    up p n | n < p      = (p+1)
+           | otherwise  = n
 
 
+upwards :: [Pitch3] -> [Pitch3]
+upwards = mapWithPrevAns up where
+  up (P3 r1 t1 f1) current@(P3 r2 t2 f2) 
+    | r2 < r1   = P3 (r2 `raiseAbove` r1) (t2 `raiseAbove` t1) (f2 `raiseAbove` f1)
+    | otherwise = current
+
+raiseAbove :: Pitch -> Pitch -> Pitch
+p1 `raiseAbove` p2 | p1 >= p2 = p1
+                   | otherwise = (addOve p1) `raiseAbove` p2
+  where
+    addOve (Pitch l a ove) = Pitch l a (ove+1)
+
+mapWithPrevAns :: (a -> a -> a) -> [a] -> [a]
+mapWithPrevAns f (x:xs) = x : step x xs 
+  where
+    step _ []     = []
+    step p (a:as) = let ans = f p a in ans : step ans as
+
+mapWithPrev _ xs       = xs   -- catch zero or one
 
 
--- root probably needs (re-)spelling...
-
-pitches :: Triad -> (Pitch,Pitch,Pitch)
-pitches (Triad root mode) = step mode where
-  step Pos = (pch, addMajorThird pch, addPerfectFifth pch)
-  step Neg = (pch, addMinorThird pch, addPerfectFifth pch)
+pitches :: SpellingMap -> Triad -> Pitch3
+pitches spellings (Triad root mode) = step mode where
+  step Pos = P3 pch (addMajorThird pch) (addPerfectFifth pch)
+  step Neg = P3 pch (addMinorThird pch) (addPerfectFifth pch)
 
   (PitchLabel l a) = pitchLabel root
-  pch              = Pitch l a 4 
+  pch              = spell spellings (Pitch l a 4 )
 
 
 pitchLabel :: Z12 -> PitchLabel
