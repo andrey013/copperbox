f3df3ce48262ef66c391da8b518247b53f6ab06b
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2010-05-16 17:51:13 +0000

    wumpus-extra - more work on shapes, anchor points.

--- libs/wumpus-extra/demo/ShapesPic.hs
+++ libs/wumpus-extra/demo/ShapesPic.hs
@@ -6,6 +6,7 @@ module ShapesPic where
 import Wumpus.Core  hiding ( center )
 import Wumpus.Extra hiding ( rectangle )
 import Wumpus.Extra.Shapes
+import Wumpus.Extra.SVGColours
 
 -- Note 
 -- Processing has (0,0) at top left, 
@@ -24,10 +25,25 @@ test01 = do
 
 
 picture1 :: Picture Double
-picture1 = rect1 ->- rect2
+picture1 = rect1 ->- rect2 ->- rect3
   where
-    rect1 = frame $ strokeRectangle $ r1
-
-    rect2 = frame $ strokeRectangle $ rotate45About (center r1) r1
-
+    rect1 = frame $ strokeRectangle () $ r1
+    rect2 = coorda r2
+    rect3 = coorda r1
     r1    = rectangle 100 50 (P2 50 25)
+    r2    = rotate45About (center r1) r1
+
+coorda :: (Fractional u , Ord u) => Rectangle u -> Picture u
+coorda rect = frameMulti 
+    [ strokeRectangle () $ rect
+    , drawCoordinate red    $ coordinate (center rect)
+    , drawCoordinate green  $ coordinate (north rect)
+    , drawCoordinate blue   $ coordinate (south rect)
+    , drawCoordinate brown  $ coordinate (east rect)
+    , drawCoordinate salmon $ coordinate (west rect)
+    , drawCoordinate indigo $ coordinate (northeast rect)
+    , drawCoordinate indigo $ coordinate (southeast rect)
+    , drawCoordinate indigo $ coordinate (southwest rect)
+    , drawCoordinate indigo $ coordinate (northwest rect)
+
+    ] 
--- libs/wumpus-extra/src/Wumpus/Extra/Shapes.hs
+++ libs/wumpus-extra/src/Wumpus/Extra/Shapes.hs
@@ -20,10 +20,19 @@
 module Wumpus.Extra.Shapes
   ( 
 
-  -- * Data types
-    Rectangle
+  -- * Anchors
+    AnchorCenter(..)
+  , AnchorCardinal(..)
+
+  -- * Coordinate (drawable point)
+  , Coordinate
+
+  , coordinate
+  , drawCoordinate
+
+  -- * Rectangle
+  , Rectangle
   , rectangle
-  , center
   , strokeRectangle
 
   ) where
@@ -40,32 +49,142 @@ import Data.VectorSpace
 
 type CTM u = Matrix3'3 u
 
-rotateCTM :: (Floating u, Real u) => Radian -> CTM u -> CTM u
-rotateCTM r m = rotationMatrix r * m
+translateCTM :: Num u => u -> u -> CTM u -> CTM u
+translateCTM x y m = translationMatrix x y * m
 
-rotateAboutCTM :: (Floating u, Real u) => Radian -> Point2 u -> CTM u -> CTM u
+scaleCTM :: Num u => u -> u -> CTM u -> CTM u
+scaleCTM x y m = scalingMatrix x y * m
+
+rotateCTM       :: (Floating u, Real u) => Radian -> CTM u -> CTM u
+rotateCTM r m   = rotationMatrix r * m
+
+rotateAboutCTM  :: (Floating u, Real u) => Radian -> Point2 u -> CTM u -> CTM u
 rotateAboutCTM r pt m = originatedRotationMatrix r pt * m
 
 
+
+
+
+--------------------------------------------------------------------------------
+-- Anchors
+
+class AnchorCenter a where
+  center :: DUnit a ~ u => a -> Point2 u
+
+class AnchorCardinal a where
+  north :: DUnit a ~ u => a -> Point2 u
+  south :: DUnit a ~ u => a -> Point2 u
+  east  :: DUnit a ~ u => a -> Point2 u
+  west  :: DUnit a ~ u => a -> Point2 u
+
+  northeast :: DUnit a ~ u => a -> Point2 u
+  southeast :: DUnit a ~ u => a -> Point2 u
+  southwest :: DUnit a ~ u => a -> Point2 u
+  northwest :: DUnit a ~ u => a -> Point2 u
+
 --------------------------------------------------------------------------------
 
-center :: forall u . (Num u, Fractional u) => Rectangle u -> Point2 u
-center rtangle = (ctm *#) $  bl .+^ v  
-  where
-    bl        = rect_bottom_left rtangle
-    tr        = rect_upper_right rtangle
-    ctm       = rect_ctm         rtangle
-    v         :: Vec2 u
-    v         = (tr .-. bl) ^* 0.5
+-- | Coordinates
 
+data Coordinate u = Coordinate
+      { coord_center          :: Point2 u
+      , coord_ctm             :: CTM u 
+      }
+
+type instance DUnit (Coordinate u) = u
+
+-- helper - extract coord_center w.r.t. the CTM 
+--
+inCtxCoord :: Num u => Coordinate u -> (Point2 u -> a) -> a
+inCtxCoord coord f = f $ ctm *# pt
+    where
+      ctm       = coord_ctm    coord
+      pt        = coord_center coord
+
+ 
+
+-- Instances 
+
+instance Num u => AnchorCenter (Coordinate u) where
+  center c = inCtxCoord c id
+
+
+instance (Floating u, Real u) => Rotate (Coordinate u) where
+  rotate r = pstar (\m s -> s { coord_ctm = rotateCTM r m }) coord_ctm 
+
+instance (Floating u, Real u) => RotateAbout (Coordinate u) where
+  rotateAbout r pt = 
+      pstar (\m s -> s { coord_ctm = rotateAboutCTM r pt m }) coord_ctm
+
+instance Num u => Scale (Coordinate u) where
+  scale x y = pstar (\m s -> s { coord_ctm = scaleCTM x y m }) coord_ctm
+
+instance Num u => Translate (Coordinate u) where
+  translate x y = 
+     pstar (\m s -> s { coord_ctm = translateCTM x y m }) coord_ctm
+
+
+--
+
+coordinate :: Num u => Point2 u -> Coordinate u
+coordinate pt = Coordinate pt identityMatrix
+
+drawCoordinate :: (Fractional u, Ellipse t) => t -> Coordinate u -> Primitive u
+drawCoordinate t coord = ellipse t 2 2 (inCtxCoord coord id)
+
+--------------------------------------------------------------------------------
+
+-- | Rectangles.
+--
 data Rectangle u = Rectangle 
-       { rect_bottom_left     :: Point2 u
-       , rect_upper_right     :: Point2 u
-       , rect_ctm             :: CTM u
-       }
+      { rect_bottom_left      :: Point2 u
+      , rect_upper_right      :: Point2 u
+      , rect_ctm              :: CTM u
+      }
 
 type instance DUnit (Rectangle u) = u
 
+
+inCtxRect :: Num u
+          => Rectangle u -> (CTM u -> Point2 u -> Vec2 u -> a) -> a
+inCtxRect rtangle f = f ctm bl (tr .-. bl)
+    where
+      ctm       = rect_ctm rtangle
+      bl        = rect_bottom_left rtangle
+      tr        = rect_upper_right rtangle
+
+-- Instances 
+  
+
+instance (Fractional u) => AnchorCenter (Rectangle u) where
+  center rtangle = inCtxRect rtangle $ \ ctm bl v -> 
+      let v' = v ^* 0.5 in (ctm *#) $ (bl .+^ v')
+
+
+
+instance (Fractional u) =>  AnchorCardinal (Rectangle u) where
+  north rtangle = inCtxRect rtangle $ \ ctm bl (V2 w h) -> 
+      (ctm *#) $ bl .+^ (V2 (w*0.5) h)
+
+  south rtangle = inCtxRect rtangle $ \ ctm bl (V2 w _) -> 
+      (ctm *#) $ bl .+^ (V2 (w*0.5) 0)
+
+  east rtangle = inCtxRect rtangle $ \ ctm bl (V2 w h) -> 
+      (ctm *#) $ bl .+^ (V2 w (h*0.5))
+
+  west rtangle = inCtxRect rtangle $ \ ctm bl (V2 _ h) -> 
+      (ctm *#) $ bl .+^ (V2 0 (h*0.5))
+
+  northeast rtangle = inCtxRect rtangle $ \ ctm bl v -> 
+      (ctm *#) $ bl .+^ v
+
+  southeast rtangle = inCtxRect rtangle $ \ ctm bl (V2 w _) -> 
+      (ctm *#) $ bl .+^ (V2 w 0)
+
+  southwest rtangle = inCtxRect rtangle $ \ ctm bl _ -> ctm *# bl
+
+  northwest rtangle = inCtxRect rtangle $ \ ctm bl (V2 _ h) -> 
+      (ctm *#) $ bl .+^ (V2 0 h)
 
 
 instance (Floating u, Real u) => Rotate (Rectangle u) where
@@ -75,6 +194,12 @@ instance (Floating u, Real u) => RotateAbout (Rectangle u) where
   rotateAbout r pt = 
       pstar (\m s -> s { rect_ctm = rotateAboutCTM r pt m }) rect_ctm
 
+instance Num u => Scale (Rectangle u) where
+  scale x y = pstar (\m s -> s { rect_ctm = scaleCTM x y m }) rect_ctm
+
+instance Num u => Translate (Rectangle u) where
+  translate x y = 
+     pstar (\m s -> s { rect_ctm = translateCTM x y m }) rect_ctm
 
 
 -- | @rectangle : width * height * center_pt -> rectangle@
@@ -87,14 +212,11 @@ rectangle w h ctr = Rectangle (ctr .-^ v) (ctr .+^ v) identityMatrix
 
 -- | make Picture or Primitive?
 --
-strokeRectangle :: (Num u, Ord u) 
-                => Rectangle u -> Primitive u
-strokeRectangle rtangle = 
-    cstroke () $ vertexPath $ pointwise (ctm *#) [bl,br,tr,tl]
+strokeRectangle :: (Fractional u, Ord u, Stroke t) 
+                => t -> Rectangle u -> Primitive u
+strokeRectangle t rtangle = cstroke t $ vertexPath [bl,br,tr,tl]
   where
-    bl        = rect_bottom_left rtangle
-    tr        = rect_upper_right rtangle
-    ctm       = rect_ctm         rtangle
-    (V2 w h)  = tr .-. bl
-    br        = bl .+^ (hvec w)
-    tl        = bl .+^ (vvec h) 
+    bl        = southwest rtangle
+    tr        = northeast rtangle
+    br        = southeast rtangle
+    tl        = northwest rtangle
