abc6985ec64bf6ea366f1e63e4cd816dbfa65f55
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2010-09-21 11:02:16 +0000

    wumpus-basic - some new work on Chains.

--- wumpus/wumpus-basic/src/Wumpus/Basic/Graphic/Chain.hs
+++ wumpus/wumpus-basic/src/Wumpus/Basic/Graphic/Chain.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE ExistentialQuantification  #-}
+{-# LANGUAGE RankNTypes                 #-}
 {-# OPTIONS -Wall #-}
 
 --------------------------------------------------------------------------------
@@ -14,78 +12,175 @@
 -- Stability   :  unstable
 -- Portability :  GHC 
 --
--- Supply points in an iterated chain.
+-- Generate points in an iterated chain.
 --
--- WARNING - this module is likely to change. The monadic version 
--- seems too /heavy/ to want to use, generating lists then 
--- zipping them might be more appropriate.
+-- WARNING - very unstable.
 --
 --------------------------------------------------------------------------------
 
 module Wumpus.Basic.Graphic.Chain
   (
+    Chain
+  , unchain
+  
+  , IterSequence
+  , iteration
+  , bounded
+  , pairOnXs
+  , pairOnYs
+  , univariateX
+  , univariateY
 
-    ChainT
-  , runChainT
-  , horizontal
+  , tableDown
+  , tableRight
 
   ) where
 
-import Wumpus.Basic.Graphic.BaseClasses
-import Wumpus.Basic.Graphic.BaseTypes
 
 import Wumpus.Core                              -- package: wumpus-core
-       
-import Control.Applicative
-import Control.Monad
 
 
-
-newtype ChainT u m a = ChainT { 
-          getChainT :: Point2F u -> Point2 u -> m (a, Point2 u) }
-
-
-type instance MonUnit (ChainT u m) = u
-
-
-
-instance Monad m => Functor (ChainT u m) where
-  fmap f ma = ChainT $ \rf s -> 
-                getChainT ma rf s >>= \(a,s1) -> return (f a, s1)
+-- | Chains are built as unfolds - AnaAlg avoids the pair 
+-- constructor in the usual definition of unfoldr and makes the
+-- state strict.
+--
+-- It is expected that all Chains built on unfolds will terminate. 
+--
+data AnaAlg st a = Done | Step a !st
 
 
-instance Monad m => Applicative (ChainT u m) where
-  pure a    = ChainT $ \_  s -> return (a,s)
-  mf <*> ma = ChainT $ \rf s -> getChainT mf rf s  >>= \(f,s1) ->
-                                getChainT ma rf s1 >>= \(a,s2) ->
-                                return (f a, s2)
+-- | IterAlg is a variant of AnaAlg that builds infinite 
+-- sequences (iterations).
+-- 
+-- When lifted to a Chain an iteration is bounded by a count so
+-- it will terminate.
+--
+data IterAlg st a = IterStep a !st 
 
 
-instance Monad m => Monad (ChainT u m) where
-  return a = ChainT $ \_  s -> return (a,s)
-  m >>= k  = ChainT $ \rf s -> getChainT m rf s >>= \(a,s1) ->
-                               (getChainT . k) a rf s1
-                                     
+data Chain ux uy u = forall st. Chain
+      { proj_x      :: ux -> u
+      , proj_y      :: uy -> u
+      , st_zero     :: st
+      , gen_step    :: st -> AnaAlg st (ux,uy)
+      }
 
-
-instance Monad m => PointSupplyM (ChainT u m) where
-  position = ChainT $ \rf s -> return (s, rf s)
-
-runChainT :: Monad m 
-          => Point2F u -> Point2 u -> ChainT u m a -> m a
-runChainT f pt0 ma = liftM fst $ getChainT ma f pt0
-
-horizontal :: (Num u, Monad m)
-           => Point2 u -> u -> ChainT u m a -> m a
-horizontal pt0 dx ma = runChainT f pt0 ma
+unchain :: Chain ux uy u -> [Point2 u]
+unchain (Chain { proj_x = fX, proj_y = fY, st_zero = st0, gen_step = step }) = 
+    go $ step st0
   where
-    f (P2 x y) = P2 (x+dx) y
+    go Done     = []
+    go (Step (x,y) next) = P2 (fX x) (fY y) : go (step next)
 
 
-instance (Monad m, TraceM m, u ~ MonUnit m) => TraceM (ChainT u m) where
-  trace a = ChainT $ \_ s -> trace a >> return ((), s)
+data IterSequence a = forall st. IterSequence
+      { initial_st  :: st
+      , iter_step   :: st -> IterAlg st a
+      }
+
+iteration :: (a -> a) -> a -> IterSequence a
+iteration fn s0 = IterSequence { initial_st = s0, iter_step = step }
+  where
+    step s = IterStep s (fn s)
 
 
-instance (Monad m, DrawingCtxM m) => DrawingCtxM (ChainT u m) where
-  askCtx         = ChainT $ \_ s -> askCtx >>= \ctx -> return (ctx, s)
-  localCtx cF ma = ChainT $ \r s -> localCtx cF (getChainT ma r s)
+bounded :: Int -> IterSequence (ux,uy) -> (ux->u) -> (uy->u) -> Chain ux uy u
+bounded n (IterSequence a0 fn) fX fY =
+    Chain { proj_x      = fX
+          , proj_y      = fY
+          , st_zero     = (0,a0)
+          , gen_step    = gstep  }
+  where
+    gstep (i,s) | i < n = let (IterStep ans next) = fn s in Step ans (i+1,next)
+    gstep _             = Done
+
+
+
+
+pairOnXs :: (ux -> uy) -> IterSequence ux -> IterSequence (ux,uy)
+pairOnXs fn (IterSequence { initial_st = s0, iter_step = step }) = 
+    IterSequence s0 step2
+  where
+    step2 s = let (IterStep a s') = step s in IterStep (a, fn a) s'
+
+
+pairOnYs :: (r -> l) -> IterSequence r -> IterSequence (l,r) 
+pairOnYs fn (IterSequence { initial_st = s0, iter_step = step }) = 
+    IterSequence s0 step2
+  where
+    step2 s = let (IterStep a s') = step s in IterStep (fn a, a) s'
+
+
+-- Note - should this be a type representing:
+--
+-- > ScaleX -> ScaleY -> Chain
+--
+-- After all, the use-case in mind always constructs it as a
+-- partial function.
+-- 
+univariateX :: (Fractional uy, Num ux) 
+            => [ux] -> (ux -> u) -> (uy -> u) -> Chain ux uy u
+univariateX zs fX fY = 
+    Chain { proj_x      = fX
+          , proj_y      = fY
+          , st_zero     = (0,zs)
+          , gen_step    = gstep  }
+  where
+    gstep (_,[])     = Done 
+    gstep (n,x:xs)   = Step (x,n) (n+i,xs)
+    len              = length zs
+    i                = rescale 0 1 0 (fromIntegral $ len-1) 1
+
+
+univariateY :: (Fractional ux, Num uy) 
+            => [uy] -> (ux -> u) -> (uy -> u) -> Chain ux uy u
+univariateY zs fX fY = 
+    Chain { proj_x      = fX
+          , proj_y      = fY
+          , st_zero     = (0,zs)
+          , gen_step    = gstep  }
+  where
+    gstep (_,[])     = Done 
+    gstep (n,y:ys)   = Step (n,y) (n+i,ys)
+    len              = length zs
+    i                = rescale 0 1 0 (fromIntegral $ len-1) 1
+
+
+
+--------------------------------------------------------------------------------
+-- Tables
+
+
+tableDown :: Num u => Int -> Int -> Chain Int Int u 
+tableDown rows cols = 
+    bounded (rows*cols) (iteration (downstep rows) (0,rows-1)) fromIntegral fromIntegral
+
+
+downstep :: Int -> (Int,Int) -> (Int,Int)
+downstep row_count (x,y) | y == 0 = (x+1,row_count-1)
+downstep _         (x,y)          = (x,y-1)
+
+
+
+tableRight :: Num u => Int -> Int -> Chain Int Int u 
+tableRight rows cols = 
+    bounded (rows*cols) (iteration (rightstep cols) (0,rows-1)) fromIntegral fromIntegral
+
+
+rightstep :: Int -> (Int,Int) -> (Int,Int)
+rightstep col_count (x,y) | x == (col_count-1) = (0,y-1)
+rightstep _         (x,y)                      = (x+1,y)
+
+
+
+--------------------------------------------------------------------------------
+-- general helpers
+
+rescale :: Fractional a => a -> a -> a -> a -> a -> a
+rescale outmin outmax innmin innmax a = 
+    outmin + innpos * (outrange / innrange)  
+  where
+    outrange = outmax - outmin
+    innrange = innmax - innmin
+    innpos   = a - innmin 
+
