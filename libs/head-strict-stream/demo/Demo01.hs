7e320da4bb2a32012ef076d9e68ddd81fcbde60a
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2011-01-01 21:05:52 +0000

    head-strict-stream - more work, including instances of the Numeric classes.

--- libs/head-strict-stream/demo/Demo01.hs
+++ libs/head-strict-stream/demo/Demo01.hs
@@ -5,28 +5,24 @@ module Demo01 where
 
 import Data.HeadStrictStream
 
-import Prelude hiding ( head, repeat, take, const, map, iterate, drop )
+import Prelude hiding ( head, repeat, take, const, map, iterate, drop
+                      , dropWhile, filter, zip, zipWith, unzip )
 
 
 demo01 :: Int
 demo01 = head $ repeat 3
 
-
 demo02 :: [Int]
 demo02 = take 10 $ const 1
-
 
 demo03 :: Stream Char
 demo03 = map (\_ -> '*') $ const (1::Int)
 
-
 demo04 :: Stream Int
 demo04 = cons 999 $ repeat 1
 
-
 demo05 :: Stream Int
 demo05 = intersperse 999 $ repeat 1
-
 
 demo06 :: Stream Int
 demo06 = interleave (repeat 999) (repeat 0)
@@ -34,6 +30,31 @@ demo06 = interleave (repeat 999) (repeat 0)
 demo07 :: Stream Int
 demo07 = iterate (+1) 0
 
-
 demo08 :: Stream Int
 demo08 = drop 5 $ iterate (+1) 0
+
+demo09 :: Stream Int
+demo09 = dropWhile (<5) $ iterate (+1) 0
+
+demo10 :: Stream Int
+demo10 = filter odd $ iterate (+1) 0
+
+demo11 :: Stream (Int,Char)
+demo11 = zip (iterate (+1) 0) (repeat 'a')
+
+demo12 :: Stream Int
+demo12 = zipWith (+) (iterate (+1) 0)  (iterate (+1) 0)
+
+demo13 :: (Stream Int, Stream Int)
+demo13 = branch (iterate (+1) 0)
+
+demo14 :: (Stream Int, Stream Char)
+demo14 = unzip $ zip (iterate (+1) 0) (repeat 'a')
+
+
+demo15 :: (Stream Int, Stream Int)
+demo15 = partition even $ iterate (+1) 0
+
+
+demo16 :: Stream Int
+demo16 = (iterate (+1) 0) + 1
--- libs/head-strict-stream/src/Data/HeadStrictStream.hs
+++ libs/head-strict-stream/src/Data/HeadStrictStream.hs
@@ -34,15 +34,21 @@ module Data.HeadStrictStream
   , unfold
   , take
   , drop
+  , dropWhile
+  , filter
+  , partition
+  , zip
+  , zipWith
+  , unzip
 
   , const
-
+  , branch
 
   ) where 
 
-import Prelude ( Functor(..), ($), id, error, Eq(..), Ord(..), Num(..), 
-                 Fractional(..),
-                 foldr, Int, (<=), (-), (.) )
+import Prelude hiding ( head, tail, map, iterate, repeat, cycle
+                      , take, drop, dropWhile, filter, zip, zipWith, unzip
+                      , const )
 
 import Data.Monoid
 import Text.Show
@@ -84,6 +90,69 @@ instance Show a => Show (Stream a) where
     where
       body = take 10 $ map shows s
 
+
+instance Ord a => Ord (Stream a) where
+  compare s1 s2 = go (viewl s1) (viewl s2)
+    where 
+      go (a :< as) (b :< bs) = case compare a b of
+                                 LT -> LT
+                                 GT -> GT
+                                 EQ -> go (viewl as) (viewl bs)
+
+
+
+-- | Note - proving equality might take a very long time...
+--
+instance Eq a => Eq (Stream a) where
+  (==) s1 s2 = go (viewl s1) (viewl s2)
+    where
+      go (a :< as) (b :< bs) | a /= b = False
+                             | otherwise = go (viewl as) (viewl bs) 
+
+
+  (/=) s1 s2 = go (viewl s1) (viewl s2)
+    where
+      go (a :< as) (b :< bs) | a /= b    = True
+                             | otherwise = go (viewl as) (viewl bs) 
+                     
+
+
+-- | Partial instance for Enum.
+--
+instance Enum a => Enum (Stream a) where
+  toEnum i          = repeat (toEnum i)
+  fromEnum _        = error $ "HeadStrictStream.fromEnum - not defined"
+
+
+instance Num a => Num (Stream a) where
+  (+)               = zipWith (+)
+  (-)               = zipWith (-) 
+  (*)               = zipWith (*)
+  abs               = map abs
+  negate            = map negate
+  signum            = map signum
+  fromInteger i     = repeat (fromInteger i)
+
+
+-- | Dummy instance for Real.
+--
+instance Real a => Real (Stream a) where
+  toRational        =  error "HeadStrictStream.toRational - not defined"
+
+
+-- | Partial instance for Enum (no toInteger).
+--
+instance Integral a => Integral (Stream a) where
+  div               = zipWith div
+  mod               = zipWith mod
+  quotRem p q       = unzip (zipWith quotRem p q)
+  toInteger _       = error $ "HeadStrictStream.toInteger - not defined"
+
+instance Fractional a => Fractional (Stream a) where
+  (/)               = zipWith (/)
+  recip             = map recip
+  fromRational r    = repeat (fromRational r)
+
 --------------------------------------------------------------------------------
 
 
@@ -93,8 +162,8 @@ cons z (Stream phi s0) = Stream chi (SP C1 s0)
     -- chi starts in C1, after first step remains in C2
     chi (SP C1 st) = Yield z (SP C2 st)
     chi (SP C2 st) = case phi st of
-                       Yield a st' -> Yield a (SP C2 st') 
-                       Skip st'    -> Skip (SP C2 st')
+        Yield a s' -> Yield a (SP C2 s') 
+        Skip s'    -> Skip (SP C2 s')
 
 
 head :: Stream a -> a
@@ -106,8 +175,8 @@ head (Stream phi s0) = go (phi s0)
 tail :: Stream a -> Stream a 
 tail (Stream phi s0) = go (phi s0)
   where
-    go (Skip s1)    = go (phi s1)
-    go (Yield _ s1) = Stream phi s1
+    go (Skip st)    = go (phi st)
+    go (Yield _ st) = Stream phi st
 
 
 
@@ -116,16 +185,16 @@ map :: (a -> b) -> Stream a -> Stream b
 map f (Stream phi s0) = Stream chi s0
   where
     chi st  = case phi st of
-                Yield a s1 -> Yield (f a) s1
-                Skip s1    -> Skip s1
+                Yield a s' -> Yield (f a) s'
+                Skip s'    -> Skip s'
 
 
 intersperse :: a -> Stream a -> Stream a
 intersperse z (Stream phi s0) = Stream chi (SP C1 s0)
   where
     chi (SP C1 st) = case phi st of
-        Yield a s1 -> Yield a (SP C2 s1)   -- switch to C2
-        Skip s1    -> Skip (SP C1 s1)      -- remain in C1
+        Yield a s' -> Yield a (SP C2 s')   -- switch to C2
+        Skip s'    -> Skip (SP C1 s')      -- remain in C1
 
     chi (SP C2 st) = Yield z (SP C1 st)
 
@@ -137,12 +206,12 @@ interleave :: Stream a -> Stream a -> Stream a
 interleave (Stream aphi as0) (Stream bphi bs0) = Stream chi (SP3 C1 as0 bs0)
   where
     chi (SP3 C1 ast bst)  = case aphi ast of
-        Yield a s1 -> Yield a (SP3 C2 s1 bst)   -- switch to C2
-        Skip s1    -> Skip (SP3 C1 s1 bst)      -- remain in C1
+        Yield a s' -> Yield a (SP3 C2 s' bst)   -- switch to C2
+        Skip s'    -> Skip (SP3 C1 s' bst)      -- remain in C1
 
     chi (SP3 C2 ast bst)  = case bphi bst of
-        Yield a s1 -> Yield a (SP3 C1 ast s1)   -- switch to C1
-        Skip s1    -> Skip (SP3 C2 ast s1)      -- remain in C2
+        Yield a s' -> Yield a (SP3 C1 ast s')   -- switch to C1
+        Skip s'    -> Skip (SP3 C2 ast s')      -- remain in C2
 
 
 iterate :: (a -> a) -> a -> Stream a
@@ -169,7 +238,7 @@ cycle xs0 = Stream phi xs0
 
 
 unfold :: (st -> (a,st)) -> st -> Stream a
-unfold f st0 = Stream phi st0
+unfold f s0 = Stream phi s0
   where
     phi st = let (a,st') = f st in Yield a st'
 
@@ -177,8 +246,8 @@ take :: Int -> Stream a -> [a]
 take len (Stream phi s0) = go len (phi s0)
   where
     go n _            | n < 1 = []
-    go n (Yield a s1)         = a : go (n-1) (phi s1)
-    go n (Skip s1)            = go n (phi s1)
+    go n (Yield a s')         = a : go (n-1) (phi s')
+    go n (Skip s')            = go n (phi s')
 
 
 drop :: Int -> Stream a -> Stream a
@@ -194,6 +263,88 @@ drop len (Stream phi s0) = Stream chi (SP3 C1 len s0)
         Yield a s' -> Yield a (SP3 C2 n s')             -- remain in C2
 
 
+dropWhile :: (a -> Bool) -> Stream a -> Stream a
+dropWhile test (Stream phi s0) = Stream chi (SP C1 s0)
+  where
+    chi (SP C1 st) = case phi st of 
+        Skip s'    -> Skip (SP C1 s')
+        Yield a s' -> if test a then Skip (SP C1 s') else Yield a (SP C2 s')
+     
+    chi (SP C2 st) = case phi st of 
+        Skip s'    -> Skip (SP C2 s')
+        Yield a s' -> Yield a (SP C2 s')
+    
+
+filter :: (a -> Bool) -> Stream a -> Stream a
+filter test (Stream phi s0) = Stream chi s0
+  where
+    chi st = case phi st of
+        Yield a s' -> if test a then Yield a s' else chi s'
+        Skip s'    -> chi s'
+
+
+partition :: (a -> Bool) -> Stream a -> (Stream a, Stream a)
+partition test (Stream phi s0) = (Stream lchi s0, Stream rchi s0)
+  where
+    lchi st = case phi st of
+        Yield a s' -> if test a then Yield a s' else lchi s'
+        Skip s'    -> lchi s'
+
+    rchi st = case phi st of
+        Yield a s' -> if not (test a) then Yield a s' else rchi s'
+        Skip s'    -> rchi s'
+
+
+zip :: Stream a -> Stream b -> Stream (a,b)
+zip (Stream aphi as0) (Stream bphi bs0) = Stream chi (SP as0 bs0)
+  where
+    chi (SP ast bst) = case aphi ast of
+        Yield a s' -> goB a s' (bphi bst)
+        Skip s'    -> chi (SP s' bst) 
+
+    goB a ast (Yield b bst) = Yield (a,b) (SP ast bst)
+    goB a ast (Skip bst)    = goB a ast (bphi bst)
+
+
+
+
+zipWith :: (a -> b -> c) -> Stream a -> Stream b -> Stream c
+zipWith op (Stream aphi as0) (Stream bphi bs0) = Stream chi (SP as0 bs0)
+  where
+    chi (SP ast bst) = case aphi ast of
+        Yield a s' -> goB a s' (bphi bst)
+        Skip s'    -> chi (SP s' bst) 
+
+    goB a ast (Yield b bst) = Yield (a `op` b) (SP ast bst)
+    goB a ast (Skip bst)    = goB a ast (bphi bst)
+
+
+
+unzip :: Stream (a,b) -> (Stream a, Stream b)
+unzip (Stream phi s0) = (Stream lchi s0, Stream rchi s0) 
+  where
+    lchi st = case phi st of
+        Yield (a,_) s' -> Yield a s'
+        Skip s'        -> lchi s'
+
+    rchi st = case phi st of
+        Yield (_,b) s' -> Yield b s'
+        Skip s'        -> rchi s'
+
+--------------------------------------------------------------------------------
+-- Destructing 
+
+data ViewL a = !a :< Stream a
+
+
+viewl :: Stream a -> ViewL a
+viewl (Stream phi s0) = go (phi s0)
+  where
+    go (Yield a s') = a :< (Stream phi s')
+    go (Skip s')    = go (phi s')
+
+
+--------------------------------------------------------------------------------
 
 -- | 'const' a:0:0:...
 --
@@ -202,4 +353,17 @@ const a = Stream phi a
   where
     phi e = Yield e 0
 
+-- | Branch a stream alternating the production of elements into 
+-- the left and right streams of the result pair.
+--
+branch :: Stream a -> (Stream a, Stream a)
+branch (Stream phi s0) = (Stream chi (SP C1 s0), Stream chi (SP C2 s0))
+  where
+    chi (SP C1 st) = case phi st of 
+      Yield a s' -> Yield a (SP C2 s')        -- switch to C2, yield @a@
+      Skip s'    -> chi (SP C1 s')            -- do until a yield
 
+    chi (SP C2 st) = case phi st of 
+      Yield _ s' -> Skip (SP C1 s')           -- switch to C1, drop @a@
+      Skip s'    -> chi (SP C2 s')            -- do until a yield
+    
