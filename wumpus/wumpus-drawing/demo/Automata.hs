d751b6cfe96b4e2e816d5e466d9845757839157a
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2011-02-10 15:37:55 +0000

    wumpus-drawing - new implementation of labelling.

--- wumpus/wumpus-drawing/demo/Automata.hs
+++ wumpus/wumpus-drawing/demo/Automata.hs
@@ -11,6 +11,7 @@ import Wumpus.Drawing.Extras.Grids
 import Wumpus.Drawing.Paths
 import Wumpus.Drawing.Shapes
 import Wumpus.Drawing.Text.SafeFonts
+import Wumpus.Drawing.Text.Label
 import Wumpus.Drawing.Text.LRText
 
 import FontLoaderUtils
@@ -61,41 +62,46 @@ automata_fonts :: [FontName]
 automata_fonts = map ps_font_name [ times_roman, times_italic ]
 
 makeCtx :: GlyphMetrics -> DrawingContext
-makeCtx = snapGrid 60.0 60.0 . fontFace helvetica . metricsContext 14
+makeCtx = snapGrid 60.0 60.0 . fontFace times_roman . metricsContext 14
 
 
 automata :: DCtxPicture
 automata = drawTracing $ do
-    draw $ grid (3,2) slate_gray `at` zeroPt
-    q0     <- nodei (0,0)   $ state
-    q1     <- cxdrawi  (above_right_of q0)      $ state
-    q2     <- cxdrawi  (below_right_of q0)      $ state
-    q3     <- cxdrawi  (below_right_of q1)      $ stopstate
+    q0     <- nodei (0,0)   $ state "q0"
+    q1     <- cxdrawi  (above_right_of q0)      $ state "q1"
+    q2     <- cxdrawi  (below_right_of q0)      $ state "q2"
+    q3     <- cxdrawi  (below_right_of q1)      $ stopstate "q3"
 
     s0     <- query (left_of q0)
     
-    drawrc q0 q1 $ straightconn
-    drawrc q1 q3 $ straightconn
-    drawrc q0 q2 $ straightconn
-    drawrc q2 q3 $ straightconn
-    drawc  s0 (west q0) $ straightconn
+    drawrci_ q0 q1 $ label_midway_of SE (singleLine "0") $ straightconn
+    drawrci_ q1 q3 $ label_midway_of SW (singleLine "1") $ straightconn
+    drawrci_ q0 q2 $ label_midway_of NE (singleLine "1") $ straightconn 
+    drawrci_ q2 q3 $ label_midway_of NW (singleLine "0") $ straightconn
+    drawci_  s0 (west q0) $ label_atstart_of EE (singleLine "start") straightconn
 
     return ()
 
 
+
 state :: ( Real u, Floating u, FromPtSize u) 
-      => LocImage u (Circle u)
-state = strokedShape $ circle 20
+      => String -> LocImage u (Circle u)
+state ss = 
+    localize (fontFace times_italic) $ 
+        label_center_of (singleLine ss) $ strokedShape $ circle 20
 
 
 stopstate :: ( Real u, Floating u, FromPtSize u) 
-          => LocImage u (Circle u)
-stopstate = dblStrokedShape $ circle 20
+          => String -> LocImage u (Circle u)
+stopstate ss = label_center_of lbl $ dblStrokedShape $ circle 20
+  where
+    lbl = singleLine ss 
+  
 
 
 straightconn :: (Real u, Floating u, FromPtSize u) 
-             => ConnectorGraphic u
-straightconn = ignoreAns $ rightArrow tri45 connLine
+             => ConnectorImage u (Path u)
+straightconn = rightArrow tri45 connLine
 
 
 
--- wumpus/wumpus-drawing/src/Wumpus/Drawing/Text/Label.hs
+++ wumpus/wumpus-drawing/src/Wumpus/Drawing/Text/Label.hs
@@ -19,8 +19,18 @@
 
 module Wumpus.Drawing.Text.Label
   ( 
+
+    locImageLabel
+  , label_center_of
+  , label_left_of
+
+  , connectorPathLabel
+  , label_midway_of
+  , label_atstart_of
+  , label_atend_of
     
-    Label(..)
+  -- * Probably obsolete...
+  , Label(..)
   , labelAbove
   , labelBelow
   , labelLeft
@@ -28,12 +38,59 @@ module Wumpus.Drawing.Text.Label
 
   ) where
 
+
+import Wumpus.Drawing.Paths
+
 import Wumpus.Basic.Kernel                      -- package: wumpus-basic
 
 import Wumpus.Core                              -- package: wumpus-core
 
 
 
+
+
+locImageLabel :: Floating u 
+         => (a -> Point2 u) -> RectPosition 
+         -> PosImage u z -> LocImage u a -> LocImage u a
+locImageLabel fn rpos lbl obj = promoteR1 $ \pt -> 
+    sdecorate (obj `at` pt)  (\a -> atStartPos lbl (fn a) rpos)
+
+
+label_center_of :: (Floating u, CenterAnchor a, u ~ DUnit a) 
+                => PosImage u no -> LocImage u a -> LocImage u a
+label_center_of = locImageLabel center CENTER
+
+
+label_left_of :: (Floating u, CardinalAnchor a, u ~ DUnit a) 
+              => PosImage u no -> LocImage u a -> LocImage u a
+label_left_of = locImageLabel west WW
+
+
+
+connectorPathLabel :: Floating u 
+                   => (Path u -> Point2 u) -> RectPosition 
+                   -> PosImage u z 
+                   -> ConnectorImage u (Path u) -> ConnectorImage u (Path u)
+connectorPathLabel fn rpos lbl obj = promoteR2 $ \p0 p1 -> 
+    sdecorate (connect obj p0 p1)  (\a -> atStartPos lbl (fn a) rpos)
+
+
+label_midway_of :: (Real u, Floating u) 
+                => RectPosition -> PosImage u z 
+                -> ConnectorImage u (Path u) -> ConnectorImage u (Path u)
+label_midway_of = connectorPathLabel midway_
+
+
+label_atstart_of :: (Real u, Floating u) 
+                 => RectPosition -> PosImage u z 
+                 -> ConnectorImage u (Path u) -> ConnectorImage u (Path u)
+label_atstart_of = connectorPathLabel atstart_
+
+
+label_atend_of :: (Real u, Floating u) 
+                 => RectPosition -> PosImage u z 
+                 -> ConnectorImage u (Path u) -> ConnectorImage u (Path u)
+label_atend_of = connectorPathLabel atend_
 
 
 
