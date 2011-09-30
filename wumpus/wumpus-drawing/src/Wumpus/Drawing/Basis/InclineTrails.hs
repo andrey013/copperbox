908f8aa0ad6fb9af71837cdca63845b1e37e734b
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2011-09-30 21:38:12 +0000

    wumpus-drawing - added a few more incline trails.

--- wumpus/wumpus-drawing/src/Wumpus/Drawing/Basis/InclineTrails.hs
+++ wumpus/wumpus-drawing/src/Wumpus/Drawing/Basis/InclineTrails.hs
@@ -20,6 +20,10 @@ module Wumpus.Drawing.Basis.InclineTrails
     incline_circle
   , incline_square
   , incline_rect
+  , incline_diamond
+  , incline_tube
+  , incline_chamf_rect
+
   )
   where
 
@@ -54,3 +58,48 @@ incline_rect h v1 =
                    , trail_theta_down h ang 
                    ]
            
+incline_diamond :: (Real u, Floating u) => u -> Vec2 u -> AnaTrail u
+incline_diamond h v1 = anaCatTrail zeroVec catt
+  where
+    hw   = 0.5 * vlength v1
+    hh   = 0.5 * h
+    ang  = vdirection v1
+    catt = mconcat $ map catline [ orthoVec   hw  (-hh) ang
+                                 , orthoVec   hw    hh  ang 
+                                 , orthoVec (-hw)   hh  ang 
+                                 , orthoVec (-hw) (-hh) ang 
+                                 ]
+           
+
+-- | @v1@ is the /interior/ vector.
+--
+incline_tube :: (Real u, Floating u) => u -> Vec2 u -> AnaTrail u
+incline_tube h v1 = 
+    anaCatTrail (theta_down_right hh ang) $ mconcat $
+      [ trail_theta_right base_len ang
+      , semicircleCCW vup
+      , trail_theta_left base_len ang
+      , semicircleCCW vdown
+      ]
+  where
+    hh        = 0.5 * h
+    ang       = vdirection v1 
+    base_len  = vlength v1 - h
+    vup       = avec (ang + half_pi) h
+    vdown     = avec (ang - half_pi) h
+
+
+incline_chamf_rect :: (Real u, Floating u) => u -> Vec2 u -> AnaTrail u
+incline_chamf_rect h v1 = 
+    anaCatTrail zeroVec $ mconcat $
+      [ trail_theta_down_right hh ang
+      , trail_theta_right base_len ang
+      , trail_theta_up_right hh ang
+      , trail_theta_up_left hh ang
+      , trail_theta_left base_len ang
+      , trail_theta_down_left hh ang
+      ]
+  where
+    hh        = 0.5 * h
+    ang       = vdirection v1 
+    base_len  = vlength v1 - h
