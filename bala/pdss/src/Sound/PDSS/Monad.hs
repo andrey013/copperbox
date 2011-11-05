1f7561dea1ba55b0226e65b1e90aa334c4e984bb
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2011-11-05 22:17:52 +0000

    pdss - added FormatCombinators rather than depend on PrettyPJ.

--- bala/pdss/src/Sound/PDSS/Monad.hs
+++ bala/pdss/src/Sound/PDSS/Monad.hs
@@ -25,8 +25,9 @@ module Sound.PDSS.Monad
   ) where 
 
 
+import Sound.PDSS.PDDoc
+import Sound.PDSS.Utils.FormatCombinators
 import Control.Applicative hiding ( empty ) 
-import Text.PrettyPrint.HughesPJ
 
 data St = St () -- to fill
 
@@ -44,7 +45,7 @@ instance Applicative GenMonad where
   pure a    = GenMonad $ \s -> (a,s,empty)
   mf <*> ma = GenMonad $ \s -> let (f,s1,d1) = getGenMonad mf s
                                    (a,s2,d2) = getGenMonad ma s1
-                               in (f a, s2, d1 $+$ d2)
+                               in (f a, s2, d1 `vconcat` d2)
 
 
 
@@ -52,28 +53,19 @@ instance Monad GenMonad where
   return  = pure
   m >>= k = GenMonad $ \s -> let (a,s1,d1) = getGenMonad m s
                                  (b,s2,d2) = getGenMonad (k a) s1
-                             in (b, s2, d1 $+$ d2)
+                             in (b, s2, d1 `vconcat` d2)
 
 tell :: Doc -> GenMonad ()
 tell d1 = GenMonad $ \s -> ((),s,d1)
 
 
 run :: (Int,Int,Int,Int) -> Int -> GenMonad a -> String
-run props sz ma = render (dcanvas props sz $+$ body)
+run (x,y,w,h) sz ma = 
+    runDoc (rec_canvas x y w h sz `vconcat` body)
   where
     body = let (_,_,d1) = getGenMonad ma (St ()) in d1
 
 
-dcanvas :: (Int,Int,Int,Int) -> Int -> Doc
-dcanvas (xp,yp,xsz,ysz) font_size = 
-    char '#' <> char 'N' <+> text "canvas" 
-             <+> (hsep $ map int [xp,yp,xsz,ysz,font_size]) <> char ';'
-
-
-dtext :: (Int,Int) -> String -> Doc
-dtext (xp,yp) ss = 
-    char '#' <> char 'X' <+> text "text" <+> int xp <+> int yp <+> text ss <> char ';'
-
 
 text_ :: Int -> Int -> String -> GenMonad ()
-text_ xp yp ss = tell $ dtext (xp,yp) ss
+text_ xp yp ss = tell $ rec_text xp yp ss
