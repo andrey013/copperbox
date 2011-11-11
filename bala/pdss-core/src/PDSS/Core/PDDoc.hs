ded3dff25fb730a70c270b8f3ec8a81c93b99839
stephen.tetley <stephen.tetley@a3f93ddd-802c-0410-a0fd-7df1615c21d1>
2011-11-11 22:39:30 +0000

    pdss - more work on the Doc combinators.

--- bala/pdss-core/src/PDSS/Core/PDDoc.hs
+++ bala/pdss-core/src/PDSS/Core/PDDoc.hs
@@ -24,11 +24,16 @@ module PDSS.Core.PDDoc
   , rec_canvas
   , rec_connect
   , rec_coords
-  , rec_floatatom0
+  , rec_msg
+  , rec_floatatom
   , rec_obj
   , rec_bang
+  , rec_toggle
   , rec_vu
   , rec_cnv
+  , rec_restore0
+  , rec_restore
+  , rec_symbolatom
   , rec_text
   
 
@@ -97,56 +102,117 @@ labelE = mbEmpty . srl_label
 rgbDoc :: RGBi -> Doc
 rgbDoc = int . rgbValue
 
+
+
 --------------------------------------------------------------------------------
 -- Print records
 
+-- | Array
+--
+-- > #X array [2 params] float [1 param]
+-- 
 rec_array :: String -> Int -> Bool -> Doc
 rec_array ss sz save = 
     recX "array" [text ss, int sz, text "float", intBool save]
 
+
+
+-- | Array data
+--
+-- > #A [... params]
+-- 
+-- Encoding of Doubles needs looking at.
+-- 
 rec_array_data :: [Double] -> Doc
 rec_array_data = recA . map dtruncFmt
 
--- | Top level canvas.
+-- | Canvas - top level canvas.
 --
+--
+-- > #N canvas [5 params]
+-- 
+-- Double is probably encoded ans an Int... needs checking.
 rec_canvas0 :: Int -> Int -> Int -> Int -> Int -> Doc
-rec_canvas0  x y w h font_size = 
-    recN "canvas" (map int [x,y,w,h,font_size])
+rec_canvas0 x y w h font_size = 
+    recN "canvas" [ int x
+                  , int y
+                  , int w
+                  , int h
+                  , int font_size
+                  ]
 
--- | Nested canvas.
+-- | Canvas - nested canvas
+--
+-- > #N canvas [6 params]
+-- 
+-- Double is probably encoded ans an Int... needs checking.
 --
 rec_canvas :: Int -> Int -> Int -> Int -> String -> Bool -> Doc
 rec_canvas  x y w h name ool = 
-    recN "canvas" [int x, int y, int w, int h, text name, intBool ool]
-
+    recN "canvas" [ int x
+                  , int y
+                  , int w
+                  , int h
+                  , text name
+                  , intBool ool
+                  ]
+-- | Connect
+-- 
+-- > #X connect [4 params]
+--
 rec_connect :: Int -> Int-> Int-> Int -> Doc
 rec_connect src out dst inn = 
-    recN "canvas" (map int [src, out, dst, inn])
-
+    recX "connect" [ int src
+                   , int out
+                   , int dst
+                   , int inn
+                   ]
+-- | Coords
+--
+-- > #X coords [7 params]
+--
 rec_coords :: Int -> Int -> Int -> Int -> Int -> Int -> Bool -> Doc
 rec_coords xfrom yto xto yfrom w h gop = 
-    recN "coords" [ int xfrom, int yto, int xto, int yfrom
-                  , int w, int h, intBool gop ]
+    recX "coords" [ int xfrom
+                  , int yto
+                  , int xto
+                  , int yfrom
+                  , int w
+                  , int h
+                  , intBool gop 
+                  ]
 
-rec_floatatom0 :: Int -> Int -> Int -> Int -> Double -> Double -> Doc
-rec_floatatom0 x y w h dmin dmax =
+-- | Floatatom
+--
+-- > #X floatatom [9 params]
+-- 
+-- Double is probably encoded ans an Int... needs checking.
+--
+rec_floatatom :: Int -> Int -> Int -> Double -> Double 
+              -> LabelPosition -> SRL -> Doc
+rec_floatatom x y w dmin dmax lpos srl =
     recX "floatatom" [ int x
                      , int y
                      , int w
-                     , int h
                      , dtruncFmt dmin
                      , dtruncFmt dmax
-                     , posn LEFT
-                     , mbDash Nothing
-                     , mbDash Nothing
-                     , mbDash Nothing 
+                     , posn lpos
+                     , labelD srl
+                     , recvD srl
+                     , sendD srl
                      ]
 
 
+-- | Msg
+-- 
+-- #X msg [2 params] [... params]
+-- 
+-- Variable number of args - transmitted as list of Doc.
+--
+rec_msg :: Int -> Int -> [Doc] -> Doc
+rec_msg x y args = recX "msg" (int x : int y : args)
+               
 
-rec_obj :: Int -> Int -> String -> [Doc] -> Doc
-rec_obj x y name params = 
-    recX "obj" (int x : int y : text name : params)
 
 
 
@@ -154,8 +220,7 @@ rec_obj x y name params =
 --
 -- > #X obj [2 params] bng [14 params]
 -- 
-rec_bang :: Int -> Int 
-         -> Int -> Integer -> Int -> Int 
+rec_bang :: Int -> Int -> Int -> Integer -> Int -> Int 
          -> SRL -> Int -> Int -> DisplayProps -> Doc
 rec_bang x y sz hold interrupt dflt srl xoff yoff props = 
     rec_obj x y "bng" [ int sz
@@ -175,9 +240,34 @@ rec_bang x y sz hold interrupt dflt srl xoff yoff props =
                       ]
 
 
+-- | Toggle
+-- 
+-- > #X obj [2 params] tgl [14 params]
+-- 
+rec_toggle :: Int -> Int -> Int -> Int
+           -> SRL -> Int -> Int -> DisplayProps -> Int -> Int -> Doc
+rec_toggle x y sz init_load srl xoff yoff props init_val dflt_val= 
+    rec_obj x y "tgl" [ int sz
+                      , int init_load
+                      , sendE srl
+                      , recvE srl
+                      , labelE srl
+                      , int xoff
+                      , int yoff
+                      , font $ obj_font props
+                      , int $ obj_fontsize props
+                      , rgbDoc $ obj_bgcolour props
+                      , rgbDoc $ obj_fgcolour props
+                      , rgbDoc $ obj_lblcolour props
+                      , int init_val
+                      , int dflt_val
+                      ]
+
+
 -- | VU meter
 -- 
 -- #X obj [2 params] vu [12 params]
+--
 rec_vu :: Int -> Int 
        -> Int -> Int
        -> SRL -> Int -> Int -> DisplayProps -> Bool -> Doc
@@ -202,8 +292,7 @@ rec_vu x y w h srl xoff yoff props logbool =
 -- 
 -- > #X obj [2 params] cnv [13 params]
 -- 
-rec_cnv :: Int -> Int 
-        -> Int -> Int -> Int  
+rec_cnv :: Int -> Int -> Int -> Int -> Int  
         -> SRL -> Int -> Int -> DisplayProps -> Doc
 rec_cnv x y sz w h srl xoff yoff props = 
     rec_obj x y "cnv" [ int sz
@@ -221,11 +310,68 @@ rec_cnv x y sz w h srl xoff yoff props =
                       , int 0 
                       ]
 
+-- | Obj
+-- 
+-- > #obj [3 params] [... params]
+--
+rec_obj :: Int -> Int -> String -> [Doc] -> Doc
+rec_obj x y name params = 
+    recX "obj" (int x : int y : text name : params)
 
 
--- | Note - semicolon needs manually escaping with a backslash.
+-- | Restore
+-- 
+-- > #X restore [2 params] pd [1 param]
+--
+rec_restore0 :: Int -> Int -> String -> Doc
+rec_restore0 x y ss = rec_restore x y "pd" ss
+
+
+-- | Restore
+-- 
+-- > #X restore [4 params]
+--
+rec_restore :: Int -> Int -> String -> String -> Doc
+rec_restore x y typ ss = 
+    recX "restore" [ int x
+                   , int y
+                   , text typ
+                   , text ss
+                   ]
+
+
+-- | Symbolatom
+--
+-- > #X symbolatom [9 params]
+-- 
+-- dmax and dmin might not be Doubles - needs checking.
+--
+rec_symbolatom :: Int -> Int -> Int -> Double -> Double 
+               -> LabelPosition -> SRL -> Doc
+rec_symbolatom x y w dmin dmax lpos srl =
+    recX "symbolatom" [ int x
+                      , int y
+                      , int w
+                      , dtruncFmt dmin
+                      , dtruncFmt dmax
+                      , posn lpos
+                      , labelD srl
+                      , recvD srl
+                      , sendD srl
+                      ]
+
+
+-- | Text
+-- 
+-- > #X text [3 params]
+--
+-- Note - semicolon needs manually escaping with a backslash.
 --
 rec_text :: Int -> Int -> String -> Doc
-rec_text x y ss = recX "text" [int x, int y, text ss]
+rec_text x y ss = 
+    recX "text" [ int x
+                , int y
+                , text ss
+                ]
 
 
